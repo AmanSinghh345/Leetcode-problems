fa5304bea39c076936270bfe3f2399fc461d3f7e
AmanSinghh345 <148769177+AmanSinghh345@users.noreply.github.com>
2024-09-09 22:52:33 +0530

    Time: 5 ms (35.72%), Space: 8.3 MB (71.85%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+      int low=1,high=x;
+      while(low<=high){
+        long long mid=low+(high-low)/2;
+        long long sqr=mid*mid;
+        if(sqr<=x)
+        low=mid+1;
+        else 
+        high=mid-1;
+      }
+      return high;
+    }
+};
