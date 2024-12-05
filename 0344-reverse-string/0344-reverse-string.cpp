6a48529f93031ac528432f69da0cd4f2a4a3bf8d
AmanSinghh345 <148769177+AmanSinghh345@users.noreply.github.com>
2024-12-05 11:47:00 +0530

    Time: 0 ms (100%), Space: 27.1 MB (86.36%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    void f(int l,int r,vector<char> & s){
-       if (l >= r) return; // Base case
-        swap(s[l], s[r]);
-         f(l + 1, r - 1, s); // Recursive call
+    void f(int i,int n,vector<char> & s){
+       if (i >= n/2) return; // Base case
+        swap(s[i], s[n-i-1]);
+         f(i+1,n,s); // Recursive call
       
 
     }
     void reverseString(vector<char>& s) {
         int n=s.size();
-       f(0,n-1,s);
+       f(0,n,s);
     
     }
 };
