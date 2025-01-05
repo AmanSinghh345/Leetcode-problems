2391c72cb1aae9ac8f7c13481fd13d42c8de6d71
AmanSinghh345 <148769177+AmanSinghh345@users.noreply.github.com>
2025-01-05 22:54:23 +0530

    Time: 0 ms (100%), Space: 10.3 MB (69.21%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-  void solve(vector<int> nums,vector<int> output,int index,vector<vector<int>>& ans){
-    if(index>=nums.size()){
-    
-        ans.push_back(output);
-        return;
-    }
-    //ex
-    solve(nums,output,index+1,ans);
-    //in
-    int ele=nums[index];
-    output.push_back(ele);
-    solve(nums,output,index+1,ans);
-  }
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> output;
-        int index=0;
-        sort(nums.begin(),nums.end());
-        solve(nums,output,index,ans);
+    void func(int ind,vector<int>& temp,vector<int>& nums,vector<vector<int>>& ans){
+            ans.push_back(temp);
+      for(int i=ind;i<nums.size();i++){
+        if(i!=ind && nums[i]==nums[i-1]) continue;
+        temp.push_back(nums[i]);
+        func(i+1,temp,nums,ans);
+        temp.pop_back();
 
-        sort(ans.begin(),ans.end());
-        ans.erase(unique(ans.begin(),ans.end()),ans.end());
-        return ans;        
+      }
+      return;
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+     sort(nums.begin(),nums.end());
+     vector<vector<int>> ans;
+     vector<int> temp;
+     int ind=0;
+     func(ind,temp,nums,ans);
+     return ans;
     }
 };
