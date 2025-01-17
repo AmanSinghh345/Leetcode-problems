4ee7f3bc1339050cbafc3450dc3b61cc1a7a24ec
AmanSinghh345 <148769177+AmanSinghh345@users.noreply.github.com>
2025-01-17 13:50:18 +0530

    Time: 0 ms (100%), Space: 264.2 MB (23.83%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
@@ -2,40 +2,10 @@ class Solution {
 public:
     bool doesValidArrayExist(vector<int>& derived) {
         int n=derived.size();
-        int i=0;
-        vector<int> A(n);
-        vector<int> B(n);
-        if(n==1 && derived[0]!=1) return true; 
-        if(n==1 && derived[0]==1) return false;
-        if(derived[i]==0 ){
-            A[i]=0;
-            A[i+1]=0;
-            B[i]=1;
-            B[i+1]=1;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ans^=derived[i];
         }
-        else {
-           A[i]=0;
-           A[i+1]=1;
-           B[i]=1;
-           B[i+1]=0; 
-        }
-        i++;
-        while(i<n-1){
-            if(derived[i]==0 ){
-                if(A[i]==0) A[i+1]=0;
-                if(A[i]==1) A[i+1]=1;
-                if(B[i]==0) B[i+1]=0;
-                if(B[i]==1) B[i+1]=1;
-            }
-            else {
-                 if(A[i]==0) A[i+1]=1;
-                if(A[i]==1) A[i+1]=0;
-                if(B[i]==0) B[i+1]=1;
-                if(B[i]==1) B[i+1]=0;
-            }
-            i++;
-        }
-        if(A[0]^A[n-1]==derived[n-1]) return true;
-        return false;
+        return  ans?false:true;
     }
 };
