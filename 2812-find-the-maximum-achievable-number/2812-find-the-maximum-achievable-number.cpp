3dadd3c2e9a5abb3bdf68f67a91a577be1bf205e
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2025-02-06 00:47:25 +0530

    Time: 0 ms (100%), Space: 9.7 MB (43.79%) - LeetHub

--- 2812-find-the-maximum-achievable-number/2812-find-the-maximum-achievable-number.cpp
+++ 2812-find-the-maximum-achievable-number/2812-find-the-maximum-achievable-number.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     int theMaximumAchievableX(int num, int t) {
-        while(t--){
-            num+=2;
-        }
-        return num;
+        int x=2*t;
+        return num+x;
     }
 };
