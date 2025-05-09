4fbf25cb0b78f7ddc90dde701dddf8456355bffe
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2025-05-09 15:44:21 +0530

    Time: 0 ms (100%), Space: 8.3 MB (99.22%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
     int mySqrt(int x) {
-      int low=1,high=x;
-      while(low<=high){
-        long long mid=low+(high-low)/2;
-        long long sqr=mid*mid;
-        if(sqr<=x)
-        low=mid+1;
-        else 
-        high=mid-1;
-      }
-      return high;
+        //Binary Search 
+        long long  low=0;
+        long long high=1e9;
+        long long  ans=1e9;
+        while(low<=high){
+            long long  mid=(low+high)/2;
+            if(mid*mid==x) return mid;
+            else if(mid*mid<x) {
+                ans=mid;
+                low=mid+1;
+            }
+            else {
+                high=mid-1;
+            }
+        }
+        return ans;
     }
 };
