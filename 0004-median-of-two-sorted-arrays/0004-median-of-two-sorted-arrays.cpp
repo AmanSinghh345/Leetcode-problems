18e5390aaf9d3ceaf8f9be181012de3d17dec092
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2025-05-19 12:37:32 +0530

    Time: 0 ms (100%), Space: 95.1 MB (62.53%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-       
-        vector<int> mergedArray;
-        for(int i = 0; i < n1; i++){
-            mergedArray.push_back(nums1[i]);
+        if(nums1.size()>nums2.size())
+        swap(nums1,nums2);
+        int n=nums1.size();
+        int m=nums2.size();
+        int l=0,r=n;
+        double ans=0;
+        while(l<=r){
+            int cut1=(l+r)/2;
+            int cut2=(n+m+1)/2 - cut1;
+            int left1=(cut1==0?INT_MIN:nums1[cut1-1]);
+            int left2=(cut2==0?INT_MIN:nums2[cut2-1]);
+            int right1=(cut1==n?INT_MAX:nums1[cut1]);
+            int right2=(cut2==m?INT_MAX:nums2[cut2]);
+            if(left1<=right2 and left2<=right1){
+                ans=max(left1,left2);
+                if((n+m)%2==0) ans=(ans+min(right1,right2))/2;
+                break;
+            }
+            else if(left1>right2) r=cut1-1;
+            else l=cut1+1;
         }
-        for(int i = 0; i < n2; i++){
-            mergedArray.push_back(nums2[i]);
-        }
-        
-        sort(mergedArray.begin(), mergedArray.end());
-        int n = mergedArray.size();
-        
-        if (n % 2 == 0) {
-            double median = (mergedArray[(n / 2) - 1] + mergedArray[n / 2]) / 2.0;
-            return median;
-        } else {
-            return mergedArray[n / 2];
-        }
+        return ans;
     }
 };
