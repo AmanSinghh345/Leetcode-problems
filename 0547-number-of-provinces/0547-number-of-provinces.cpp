8adf86c40304771e3cac3d741fbf568cd21ef223
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2025-08-13 14:13:48 +0530

    Time: 4 ms (30.23%), Space: 21.3 MB (10.51%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,34 +1,44 @@
 class Solution {
 public:
-    void bfs(int node,vector<vector<int>> &adjMat,int& cnt,vector<int> &vis,queue<int> &q){
-        q.push(node);
+    void dfs(vector<int>& ans,vector<int>& vis,vector<vector<int>>& adj,int node){
         vis[node]=1;
-        while(!q.empty()){
-            node=q.front();
-            q.pop();
-
-            for(int i=0;i<adjMat[node].size();i++){
-                if(adjMat[node][i]==1 and vis[i]==0){ // means connected not visited
-                        q.push(i);
-                        vis[i]=1;
-                }
+        ans.push_back(node);
+        for(int i=0;i<adj[node].size();i++)
+        {
+            if(vis[adj[node][i]]==0)
+            {
+                dfs(ans,vis,adj,adj[node][i]);
             }
         }
+        return;
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n=isConnected.size();
-        queue<int> q;
-        vector<int> vis(n,0);
-        int ans=0;
-      
-
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                ans++;
-        bfs(i,isConnected,ans,vis,q);
+        vector<vector<int>> adj(n+1);
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i!=j and isConnected[i][j]==1)
+                {
+                    int u=i+1;
+                    int v=j+1;
+                    adj[u].push_back(v);
+                    adj[v].push_back(u);
+                }
+            }
         }
+        vector<int> vis(n+1,0);
+        vector<int> ans;
+        int provinces=0;
+        for(int i=1;i<=n;i++)
+        {
+            if(vis[i]==0)
+            {   
+                provinces++;
+                dfs(ans,vis,adj,i);
+            }
         }
-
-        return ans;
+        return provinces;
     }
 };
