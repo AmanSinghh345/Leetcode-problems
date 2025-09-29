f9048fedead1eaed21f70988a24eedcb8ef69b69
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2025-09-29 16:00:32 +0530

    Time: 6 ms (70.33%), Space: 35.2 MB (66.35%) - LeetHub

--- 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
@@ -1,41 +1,43 @@
 class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
-        vector<vector<pair<int, int>>> graph(n);
-        for (const auto& road : roads) {
-            int u = road[0], v = road[1], time = road[2];
-            graph[u].emplace_back(v, time);
-            graph[v].emplace_back(u, time);
+        vector<vector<pair<int,int>>> adj(n);
+        for(const auto & it : roads)
+        {
+            int u=it[0],v=it[1],time=it[2];
+            adj[u].push_back({v,time});
+            adj[v].push_back({u,time});
         }
+        vector<long long> dist(n,LLONG_MAX);
+        vector<long long > ways(n,0);
 
-        vector<long long> dist(n, LLONG_MAX);
-        vector<int> ways(n, 0);
+        dist[0]=0;
+        ways[0]=1;
 
-        dist[0] = 0;
-        ways[0] = 1;
+        priority_queue<pair<long long ,int> , vector< pair <long long , int >> , greater<>> pq;
+        pq.emplace(0,0);
 
-        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<>> pq;
-        pq.emplace(0, 0);
-
-        const int MOD = 1e9 + 7;
-
-        while (!pq.empty()) {
-            auto [d, node] = pq.top();
+        const int mod=1e9 + 7 ;
+        while(!pq.empty())
+        {
+            auto [d,node] = pq.top();
             pq.pop();
 
-            if (d > dist[node]) continue;
-
-            for (const auto& [neighbor, time] : graph[node]) {
-                if (dist[node] + time < dist[neighbor]) {
-                    dist[neighbor] = dist[node] + time;
-                    ways[neighbor] = ways[node];
-                    pq.emplace(dist[neighbor], neighbor);
-                } else if (dist[node] + time == dist[neighbor]) {
-                    ways[neighbor] = (ways[neighbor] + ways[node]) % MOD;
+            if(d>dist[node]) continue;
+            for(const auto & [neigh,time] : adj[node])
+            {
+                if(dist[node]+time < dist[neigh])
+                {
+                    dist[neigh]=dist[node] + time ;
+                    ways[neigh] = ways[node];
+                    pq.emplace(dist[neigh],neigh);
+                }
+                else if( dist[node] + time == dist[neigh])
+                {
+                    ways[neigh]= (ways[node] + ways[neigh])%mod;
                 }
             }
         }
-
-        return ways[n - 1];
+        return ways[n-1];
     }
 };
