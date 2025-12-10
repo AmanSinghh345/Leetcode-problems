7cc5cd6cd297d7231a78607f7bd8c6764ea1274b
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2025-12-10 09:54:06 +0530

    Time: 0 ms (100%), Space: 8.6 MB (33.37%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
+    int recurr(int n ,int cnt,vector<int>& dp)
+    {
+        if(n==0) return 1;
+        if(n==1) return 1;
+        if(dp[n]!=-1) return dp[n];
+        int a=recurr(n-1,cnt,dp);
+        int b=recurr(n-2,cnt,dp);
+        return dp[n]=a+b;
+    }
     int climbStairs(int n) {
-        if(n<=2) return n;
-        int x=1,y=2;
-        for(int i=3;i<n;i++){   
-            int temp=y;   
-            y=y+x;        
-            x=temp;         
-        }
-        return (x+y);
+
+        int cnt=0;
+        vector<int> dp(n+1,-1);
+        int ans=recurr(n,cnt,dp);
+
+        return ans;
+         
     }
 };
-
-
