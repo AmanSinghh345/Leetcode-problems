9f9c3fa4fa10701d8cdeef463f461f7a0f5930b6
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2025-12-24 13:45:28 +0530

    Time: 0 ms (100%), Space: 8.2 MB (70.68%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        int rev=0;
+        for(int i=0;i<32;i++)
+        {
+
+            if((n&(1<<i))!=0)
+            {
+                rev=rev|(1<<(32-i-1));
+            }
+        }
+        return rev;
+    }
+};
