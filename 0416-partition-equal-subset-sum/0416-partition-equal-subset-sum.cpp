062fd25d5dc8296359e287a1b857042283fe7bd4
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-01-03 21:32:43 +0530

    Time: 83 ms (76.94%), Space: 13.3 MB (92.62%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,65 +1,18 @@
 class Solution {
 public:
-//RECURSIVE SOLUTION
-bool solve(vector<int>&nums , int target , int i){
-    if(target<0 || i==nums.size()) return false;
-    if(target ==0) return true;
-    //include 
-    bool inc =solve(nums, target-nums[i] ,i+1);
-    bool exc = solve( nums ,target , i+1);
-
-    return inc || exc ;
-}
-//2-D approach top-down
-bool solveMemo(vector<int>&nums , int target , int i, vector<vector<int>>&dp){
-    if(target<0 || i==nums.size()) return false;
-    if(target ==0) return true;
-    //include 
-    if(dp[target][i]!= -1) return dp[target][i];
-    bool inc =solveMemo(nums, target-nums[i] ,i+1,dp);
-    bool exc = solveMemo( nums ,target , i+1,dp);
-
-   dp[target][i] = inc || exc;
-   return dp[target][i];
-}
-// tabulation 
-bool solveTebu(vector<int>& nums, int target) {
-    int n = nums.size();
-    vector<vector<bool>> dp(target + 1, vector<bool>(n + 1, false));
-
-    // Base case: sum 0 is always possible
-    for (int i = 0; i <= n; i++) {
-        dp[0][i] = true;
-    }
-
-    for (int t = 1; t <= target; t++) {
-        for (int i = n - 1; i >= 0; i--) {
-            bool exc = dp[t][i + 1];
-            bool inc = (t >= nums[i]) ? dp[t - nums[i]][i + 1] : false;
-
-            dp[t][i] = inc || exc;
-        }
-    }
-
-    return dp[target][0];
-}
-
-
-
     bool canPartition(vector<int>& nums) {
-        int n = nums.size();
-       int sum = accumulate(nums.begin(),nums.end(),0);
-       if(sum & 1 ) return false;
-        int target = sum/2;
-        // recursion 
-        //return solve(nums,target ,0);
-
-        // top-downapproach
-          int i = 0;
-        vector<vector<int>>dp(target+1,vector<int>(n,-1));
-         return solveMemo(nums,target,i,dp);
-
-        //tabulation method
-      //return  solveTebu(nums,target);
+        int total=accumulate(nums.begin(),nums.end(),0);
+        if(total&1) return false;
+        int req=total/2;
+        vector<bool> dp(req+1,false);
+        dp[0]=true;
+        for(int x:nums)
+        {
+            for(int target=req;target>=x;target--)
+            {
+                dp[target]=dp[target]||dp[target-x];
+            }
+        }
+        return dp[req];
     }
 };
