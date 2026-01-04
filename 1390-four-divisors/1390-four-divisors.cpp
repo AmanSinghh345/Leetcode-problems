cb8e8037800518fb0459c3542bab6f1ffeeeb487
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-01-04 19:11:35 +0530

    Time: 7 ms (85.96%), Space: 16.1 MB (71.43%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.cpp
+++ 1390-four-divisors/1390-four-divisors.cpp
@@ -5,15 +5,27 @@ public:
         int ans=0;
         for(int i=0;i<n;i++)
         {
-            int cnt=1;
-            int sum=nums[i];
-            for(int j=1;j<=nums[i]/2;j++)
+            int cnt=0;
+            int sum=0;
+            for(int j=1;j*j<=nums[i];j++)
             {
-                if(nums[i]%j==0) {
-                    cnt++;
-                    sum+=j;
+                if(nums[i]%j==0) 
+                    {
+                        int f=j;
+                        int s=nums[i]/j;
+                        if(f==s)
+                        {
+                            cnt++;
+                            sum+=f;
+                        }
+                        else 
+                        {
+                            cnt+=2;
+                            sum+=(f+s);
+                        }
+                    }
                     if(cnt>4) break;
-                }
+                
             }
             if(cnt==4) ans+=sum;
         }
