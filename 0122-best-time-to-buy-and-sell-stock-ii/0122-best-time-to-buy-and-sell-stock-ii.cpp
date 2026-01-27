dfdb6603426f4863114adff0337ff119b0d89334
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-01-27 20:49:57 +0530

    Time: 0 ms (100%), Space: 20.2 MB (17.18%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -3,10 +3,11 @@ public:
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
         int ans=0;
-        for(int i=1;i<n;i++){
-            if(prices[i]>prices[i-1]){
-                ans+=(prices[i]-prices[i-1]);
-                
+        for(int i=0;i<n-1;i++)
+        {
+            if(prices[i+1]>prices[i])
+            {
+                ans+=(prices[i+1]-prices[i]);
             }
         }
         return ans;
