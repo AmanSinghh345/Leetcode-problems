d7ef4b9ca61ca5468de617489b8da480047d4ef6
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-01-30 17:34:56 +0530

    Time: 0 ms (100%), Space: 9.5 MB (28.34%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
-    int recurr(int i,int j,vector<vector<int>>& dp)
-    {
-        if(i==0 and j==0) return 1;
-        if(i<0 or j<0 ) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int up=recurr(i-1,j,dp);
-        int left=recurr(i,j-1,dp);
-        return dp[i][j]=up+left;
-    }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int> (n,-1));
-        int ans=recurr(m-1,n-1,dp);
-        return ans;
+        vector<vector<int>> dp(m+1,vector<int> (n+1,-1));
+        for(int j=0;j<=n;j++)
+        {
+            dp[0][j]=0;
+        }
+        for(int i=1;i<=m;i++)
+            dp[i][0]=0;
+
+        dp[1][1]=1;
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(i==1 and j==1) continue;
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m][n];
     }
 };
