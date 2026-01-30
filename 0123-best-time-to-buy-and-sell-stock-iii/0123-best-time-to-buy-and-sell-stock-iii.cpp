6bf61b027af78f7774a622fffc95126e9afa4bbb
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-01-30 21:09:20 +0530

    Time: 236 ms (49.72%), Space: 105.8 MB (56.52%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -4,8 +4,28 @@ public:
         int n=prices.size();
         // vector<vector<vector<int>>> dp (n,vector<vector<int>> (2,vector<int> (3,-1)));
         int dp[1000001][2][3];
-        memset(dp,-1,sizeof(dp));
-        return f(0,1,2,prices,dp);
+        memset(dp,0,sizeof(dp));
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int buy=0;buy<2;buy++)
+            {
+                for(int cap=1;cap<3;cap++)
+                {
+                    int profit=0;
+                    if(buy)
+                    {
+                        profit=max(-prices[i]+dp[i+1][0][cap],0+dp[i+1][1][cap]);
+                    }
+                    else 
+                    {
+                        profit=max(prices[i]+dp[i+1][1][cap-1],0+dp[i+1][0][cap]);
+                    }
+                    dp[i][buy][cap]=profit;
+                }
+            }
+        }
+        return dp[0][1][2];
     }
     int f(int i,int buy,int cap,vector<int>& prices,int dp[][2][3])
     {
