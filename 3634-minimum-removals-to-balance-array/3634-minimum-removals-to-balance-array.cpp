3ad80f486b8e4f6bba793f5168a2e4c2286b7c7c
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-02-07 20:54:21 +0530

    Time: 23 ms (95.91%), Space: 105.1 MB (52.63%) - LeetHub

---
+++ 3634-minimum-removals-to-balance-array/3634-minimum-removals-to-balance-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minRemoval(vector<int>& nums, int k) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        int j=0;int ans=n;
+        for(int i=0;i<n;i++)
+        {
+            while(j<n and nums[j]<=1LL*nums[i]*k)
+            {
+                j++;
+            }
+            ans=min(ans,n-(j-i));
+        }
+        return ans;
+    }
+};
