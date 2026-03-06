3c0ff45fc8fe61b8841010d7d43088c294cf4a6d
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-03-06 17:16:58 +0530

    Time: 0 ms (100%), Space: 10 MB (10.36%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.size() != t.size()) return false;
-
-        vector<int> freq(26, 0);  // Frequency array for 26 letters
-
-        // Increase the count for characters in `s` and decrease for `t`
-        for (int i = 0; i < s.size(); i++) {
-            freq[s[i] - 'a']++;
-            freq[t[i] - 'a']--;
+        int n=s.size();
+        int m=t.size();
+        if(n!=m) return false;
+        unordered_map<char,int> u1,u2;
+        for(int i=0;i<n;i++)
+        {
+            u1[s[i]]++;
+            u2[t[i]]++;
         }
-
-        // Check if all counts are zero
-        for (int i = 0; i < 26; i++) {
-            if (freq[i] != 0) return false;
+        for(auto it=u1.begin();it!=u1.end();it++)
+        {
+            if(it->second!=u2[it->first]) return false;
         }
-
         return true;
     }
 };
