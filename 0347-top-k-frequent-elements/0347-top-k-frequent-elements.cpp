2cf4a82bdb07268d197ac62b7be4edaffa493a8a
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-03-06 18:09:41 +0530

    Time: 3 ms (51.07%), Space: 17.9 MB (84.54%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int,int> umap;
-        
         for(int x:nums) umap[x]++;
-        priority_queue<pair<int,int>,vector<pair<int,int>>> pq ; // maximum by default
+
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;// min heap
+
         for(auto it:umap)
         {
             pq.push({it.second,it.first});
+            if(pq.size()>k) pq.pop();
         }
         vector<int> ans;
-        while(k--)
+        while(!pq.empty())
         {
             ans.push_back(pq.top().second);
             pq.pop();
         }
         return ans;
-
     }
 };
