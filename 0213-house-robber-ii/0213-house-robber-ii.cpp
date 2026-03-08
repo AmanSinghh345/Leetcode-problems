1585cbcea0fb0b2533c21c8ca79562e0ea16b5e0
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-03-08 17:56:25 +0530

    Time: 0 ms (100%), Space: 10.1 MB (96.88%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -2,36 +2,24 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n=nums.size();
-        int prev2=0;
-        int prev=nums[0];
         if(n==1) return nums[0];
-        for(int i=1;i<n-1;i++)
+        int prev1=0,prev2=0;
+        for(int i=0;i<=n-2;i++)
         {
-            int take=nums[i];
-            if(i>1)
-            {
-                take+=prev2;
-            }
-            int not_take=0 + prev;
-            int curi=max(take,not_take);
-            prev2=prev;
-            prev=curi;
+            int curr=max(prev1,prev2+nums[i]);
+            prev2=prev1;
+            prev1=curr;
         }
-        int val1=prev;
+        int a=prev1;
+        prev1=0;
         prev2=0;
-        prev=nums[1];
-        for(int i=2;i<n;i++)
+        for(int i=1;i<n;i++)
         {
-            int take=nums[i];
-            if(i>2){
-                take+=prev2;
-            }
-            int not_take=0 + prev;
-            int curi=max(take,not_take);
-            prev2=prev;
-            prev=curi;
+              int curr=max(prev1,prev2+nums[i]);
+            prev2=prev1;
+            prev1=curr;
         }
-        int val2=prev;
-        return max(val2,val1);
+        int b=prev1;
+        return max(a,b);
     }
 };
