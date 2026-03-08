8d9036624197665bbc37511505c2c0a1845ac286
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-03-08 17:47:25 +0530

    Time: 0 ms (100%), Space: 10.6 MB (44.28%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -10,6 +10,17 @@ public:
     }
     int rob(vector<int>& nums) {
         vector<int> dp(nums.size(),-1);
-        return f(0,nums,dp);
+        dp[0]=nums[0];
+        for(int i=1;i<nums.size();i++)
+        {
+            int take=nums[i];
+            if(i>1)
+            {
+                take+=dp[i-2];
+            }
+            int notTake=dp[i-1];
+            dp[i]=max(take,notTake);
+        }
+        return dp[nums.size()-1];
     }
 };
