9de9a5586574e05ca6220030ace913df37c898ee
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-03-16 18:49:04 +0530

    Time: 19 ms (81.23%), Space: 12.5 MB (55.73%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.cpp
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
-    int numTeams(vector<int>& ratings) {
-        int cnt=0;
-        int n=ratings.size();
-        for(int i=0;i<n-2;i++)
-        {
-            for(int j=i+1;j<n-1;j++){
-                if(ratings[j]<=ratings[i]) continue;
-                for(int k=j+1;k<n;k++){
-                    if(ratings[j]<ratings[k]) cnt++;
-                }
+    int numTeams(vector<int>& nums) {
+        int n=nums.size();
+        int ans=0;
+        for(int j=1;j<n-1;j++){
+
+            int val=nums[j];
+            int ls=0,lb=0,rs=0,rb=0;
+            for(int i=0;i<j;i++){
+                if(nums[i]<val) ls++;
+                if(nums[i]>val) lb++;
             }
-        }
-          for(int i=0;i<n-2;i++)
-        {
-            for(int j=i+1;j<n-1;j++){
-                if(ratings[j]>=ratings[i]) continue;
-                for(int k=j+1;k<n;k++){
-                    if(ratings[j]>ratings[k]) cnt++;
-                }
+            for(int k=j+1;k<n;k++){
+                if(nums[k]<val) rs++;
+                if(nums[k]>val) rb++;
             }
+            ans+=(ls*rb)+(lb*rs);
         }
-        return cnt;
+        return ans;
     }
 };
