59fd02fc412dbb70602eda5c5ff21d22b3ba82f2
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-03-23 18:34:38 +0530

    Time: 1 ms (47.31%), Space: 14.2 MB (54.26%) - LeetHub

--- 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.cpp
+++ 1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix.cpp
@@ -6,67 +6,33 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
-        vector<vector<pair<long long,long long>>> dp(
-            m, vector<pair<long long,long long>>(n, {LLONG_MIN, LLONG_MAX})
-        );
+        vector< pair<long long ,long long > > dp(n);
 
-        dp[0][0] = {grid[0][0], grid[0][0]};
+        dp[0]={grid[0][0],grid[0][0]};
 
-    
-        for(int i = 1; i < m; i++) {
-            long long val = dp[i-1][0].first * grid[i][0];
-            dp[i][0] = {val, val};
+        for(int j=1;j<n;j++){
+            long long  val=dp[j-1].first*grid[0][j];
+            dp[j]={val,val};
         }
 
+        for(int i=1;i<m;i++){
+            long long val=dp[0].first*grid[i][0];
+            dp[0]={val,val};
+            for(int j=1;j<n;j++){
 
-        for(int j = 1; j < n; j++) {
-            long long val = dp[0][j-1].first * grid[0][j];
-            dp[0][j] = {val, val};
-        }
+                long long x=grid[i][j];
+                vector<long long > candidates={
+                    x*dp[j].first,
+                    x*dp[j].second,
+                    x*dp[j-1].first,
+                    x*dp[j-1].second
+                };
+                long long maxi=*max_element(candidates.begin(),candidates.end());
+                long long mini=*min_element(candidates.begin(),candidates.end());
 
-        for(int i = 1; i < m; i++) {
-            for(int j = 1; j < n; j++) {
-                long long val = grid[i][j];
-
-                if(val == 0) {
-                    dp[i][j] = {0, 0};
-                    continue;
-                }
-
-                long long maxPos = LLONG_MIN;
-                long long maxNeg = LLONG_MAX;
-
-                // from top
-                if(dp[i-1][j].first != LLONG_MIN) {
-                    long long x = val * dp[i-1][j].first;
-                    maxPos = max(maxPos, x);
-                    maxNeg = min(maxNeg, x);
-                }
-                if(dp[i-1][j].second != LLONG_MAX) {
-                    long long x = val * dp[i-1][j].second;
-                    maxPos = max(maxPos, x);
-                    maxNeg = min(maxNeg, x);
-                }
-
-                // from left
-                if(dp[i][j-1].first != LLONG_MIN) {
-                    long long x = val * dp[i][j-1].first;
-                    maxPos = max(maxPos, x);
-                    maxNeg = min(maxNeg, x);
-                }
-                if(dp[i][j-1].second != LLONG_MAX) {
-                    long long x = val * dp[i][j-1].second;
-                    maxPos = max(maxPos, x);
-                    maxNeg = min(maxNeg, x);
-                }
-
-                dp[i][j] = {maxPos, maxNeg};
+                dp[j]={maxi,mini};
             }
         }
-
-        long long res = dp[m-1][n-1].first;
-
-        if(res < 0) return -1;
-        return res % mod;
+        return dp[n-1].first<0? -1 : dp[n-1].first%mod;
     }
 };
