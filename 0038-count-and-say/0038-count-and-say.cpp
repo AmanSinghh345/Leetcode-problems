938457fb32a4587f3e8beb6fbaf8b914339dc152
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-03-27 14:30:40 +0530

    Time: 4 ms (50.51%), Space: 10 MB (42.27%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     string countAndSay(int n) {
-        string res = "1";
-        helper(1, n, res);
-        return res;
-    }
-
-    void helper(int ind, int n, string& res) {
-        if (ind >= n)
-            return;
-        string prev_val = res;
-        res = "";
-        int left = 0, count = 0;
-        for (int right = 0; right < prev_val.size(); right++) {
-            if (prev_val[left] != prev_val[right]) {
-                res += to_string(count) + prev_val[left];
-                left = right;
-                count = 1;
-            } else {
-                count++;
+        if(n==1) return "1";
+        string prev="1";
+        
+        n-=1;
+        while(n--){
+        string curr="";
+        char last=prev[0];
+        int cnt=1;
+        for(int i=1;i<prev.size();i++){
+            if(last==prev[i]) cnt++;
+            else {
+                curr+=(to_string(cnt) + string(1,last));
+                last=prev[i];
+                cnt=1;
             }
         }
-        res += to_string(count) + prev_val[left];
-        return helper(ind + 1, n, res);
+        curr+=(to_string(cnt)+string(1,last));
+        prev=curr;
+        }
+        return prev;
     }
 };
