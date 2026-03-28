09a4250de0dd50242f77658abd3c479039cb8923
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-03-28 19:49:24 +0530

    Time: 0 ms (100%), Space: 7.9 MB (69.19%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -3,13 +3,10 @@ public:
     int climbStairs(int n) {
         if(n<=2) return n;
         int prev2=1,prev1=1;
-        int curr;
-        for(int i=2;i<=n;i++)
-        {
-            curr=prev1+prev2;
+        for(int i=2;i<=n;i++){
+            int curr=prev1+prev2;
             prev2=prev1;
             prev1=curr;
-            
         }
         return prev1;
     }
