da3a64b644c2ff3409cd731fe9807640fc69a1ad
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-03-28 19:59:10 +0530

    Time: 0 ms (100%), Space: 10.3 MB (73.99%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -4,22 +4,19 @@ public:
         int n=nums.size();
         if(n==1) return nums[0];
         int prev1=0,prev2=0;
-        for(int i=0;i<=n-2;i++)
-        {
+        for(int i=0;i<=n-2;i++){
             int curr=max(prev1,prev2+nums[i]);
             prev2=prev1;
             prev1=curr;
         }
-        int a=prev1;
-        prev1=0;
-        prev2=0;
-        for(int i=1;i<n;i++)
-        {
-              int curr=max(prev1,prev2+nums[i]);
+        int ans=prev1;
+        prev1=0,prev2=0;
+        for(int i=1;i<=n-1;i++){
+            int curr=max(prev1,prev2+nums[i]);
             prev2=prev1;
             prev1=curr;
         }
-        int b=prev1;
-        return max(a,b);
+        ans=max(ans,prev1);
+        return ans;
     }
 };
