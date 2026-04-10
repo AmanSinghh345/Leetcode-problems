5a6c1dcfcd65df74622d3bffd72590967e69a51d
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-04-10 12:49:19 +0530

    Time: 36 ms (5.09%), Space: 37.6 MB (5.26%) - LeetHub

---
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> kmp(string s){
+        int n=s.size();
+        vector<int> lps(n,0);
+        for(int i=1;i<n;i++){
+            int prev_ind=lps[i-1];
+            while(prev_ind>0 and s[i]!=s[prev_ind]) prev_ind=lps[prev_ind-1];
+            if(s[i]==s[prev_ind]) prev_ind++;
+            lps[i]=prev_ind;
+        }
+        return lps;
+    }
+    string longestPrefix(string s) {
+        string take="";
+        int n=s.size();
+        for(int i=0;i<n-1;i++){
+            take+=s[i];
+        }
+        take+="#";
+        for(int i=1;i<n;i++) {
+            take+=s[i];
+        }
+        vector<int> lps=kmp(take);
+        int sz=lps.size();
+        int len=lps[sz-1];
+        return s.substr(0,len);
+    }
+};
