11e9e31c0c68c8bde6b42c0b58a6ec682d7af54c
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-04-10 18:10:08 +0530

    Time: 7 ms (30.16%), Space: 39.1 MB (11.17%) - LeetHub

---
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode* root,vector<int>&res){
+        if(root==NULL) return ;
+        inorder(root->left,res);
+        res.push_back(root->val);
+        inorder(root->right,res);
+    }
+    bool findTarget(TreeNode* root, int k) {
+        if(root==NULL ) return false;
+        unordered_map<int,int> umap;
+        vector<int> res;
+        inorder(root,res);
+
+        int n=res.size();
+        for(int i=0;i<n;i++){
+            umap[res[i]]++;
+            if(k-res[i]==res[i]){
+                 if(umap.find(k-res[i])!=umap.end() and  umap[k-res[i]]>1) return true;
+            }
+            else {
+                if(umap.find(k-res[i])!=umap.end()) return true;
+            }
+        }
+        return false;
+
+    }
+};
