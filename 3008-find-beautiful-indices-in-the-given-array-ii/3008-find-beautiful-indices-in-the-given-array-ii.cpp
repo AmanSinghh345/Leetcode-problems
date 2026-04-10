42e6dde7195831496f08208e689f5b7c74c36ee5
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-04-10 10:14:01 +0530

    Time: 126 ms (14.97%), Space: 132.9 MB (21.09%) - LeetHub

---
+++ 3008-find-beautiful-indices-in-the-given-array-ii/3008-find-beautiful-indices-in-the-given-array-ii.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<int> kmp(string s){
+        int n=s.size();
+        vector<int> lps(n,0);
+        for(int i=1;i<n;i++){
+            int prev_ind=lps[i-1];
+            while(prev_ind>0 and s[i]!=s[prev_ind]){
+                prev_ind=lps[prev_ind-1];
+            }
+            if(s[i]==s[prev_ind]) prev_ind++;
+            lps[i]=prev_ind;
+        }
+        return lps;
+    }
+    vector<int> beautifulIndices(string s, string a, string b, int k) {
+
+        string f=a+"#"+s;
+        string sec=b+"#"+s;
+        vector<int> first=kmp(f);
+        vector<int> startingForFirst;
+        for(int i=(a.size()+1);i<first.size();i++){
+            if(first[i]==a.size()) {
+                startingForFirst.push_back(i-2*a.size());
+            }
+        }
+        vector<int> second=kmp(sec);
+        vector<int> startingForSecond;
+        for(int j=(b.size()+1);j<second.size();j++){
+            if(second[j]==b.size()){
+                startingForSecond.push_back(j-2*b.size());
+            }
+        }
+        vector<int> valid;
+        for(int i=0;i<startingForFirst.size();i++){
+            auto lb=lower_bound(startingForSecond.begin(),startingForSecond.end(),startingForFirst[i]);
+            
+            if(lb!=startingForSecond.end() and abs(*lb-startingForFirst[i])<=k){
+                valid.push_back(startingForFirst[i]);
+            }
+            else if(lb!=startingForSecond.begin()){
+                lb--;
+            if(abs(*lb-startingForFirst[i])<=k) valid.push_back(startingForFirst[i]);
+        }
+        }
+        return valid;
+    }
+};
