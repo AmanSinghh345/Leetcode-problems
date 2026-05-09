6396aef289bd7f7cad197c738f0a46a7f3c76d6d
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-05-09 13:45:54 +0530

    Time: 0 ms (100%), Space: 8.7 MB (79.28%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    void find(int i,int sum,int k,int n,vector<int>& a ,vector<int>& ds,vector<vector<int>>& ans){
+    void find(int idx,int sz,int target,vector<int>& ds,vector<vector<int>>& ans){
 
-        if(i==a.size()){
-            if(sum==n and ds.size()==k){
-                ans.push_back(ds); 
-            }
+        if(target==0 and ds.size()==sz){
+            ans.push_back(ds);
             return;
         }
-        ds.push_back(a[i]);
-        find(i+1,sum+a[i],k,n,a,ds,ans);
-        ds.pop_back();
-        find(i+1,sum,k,n,a,ds,ans);
+        //pruning
+        if(target<0 || ds.size()>sz) return ;
+
+        for(int i=idx;i<=9;i++){
+            ds.push_back(i);
+            find(i+1,sz,target-i,ds,ans);
+            ds.pop_back();
+        }
     }
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>> ans;
-        vector<int> a={1,2,3,4,5,6,7,8,9};
         vector<int> ds;
-        find(0,0,k,n,a,ds,ans);
+        find(1,k,n,ds,ans);
         return ans;
     }
 };
