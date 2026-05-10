2bb5459c7ea414421f5276fc2502e912fed942b4
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-05-10 13:12:42 +0530

    Time: 1952 ms (56.52%), Space: 568.1 MB (51.13%) - LeetHub

--- 3928-minimum-cost-to-buy-apples-ii/3928-minimum-cost-to-buy-apples-ii.cpp
+++ 3928-minimum-cost-to-buy-apples-ii/3928-minimum-cost-to-buy-apples-ii.cpp
@@ -1,54 +1,57 @@
 class Solution {
 public:
-    vector<long long > dijkstra(int n,int i,vector<vector<pair<int,long long> >>& graph){
-        const long long  INF=1e18;
+    vector<long long > dijkstra(int n,int i,vector<vector<pair<int,long long >>>& adj){
+        const long long INF=1e18;
         vector<long long> dist(n,INF);
 
-        priority_queue<pair<long long,int>,vector<pair<long long,int>>,greater<>> pq;
+        priority_queue<pair<long long ,int >,vector<pair<long long ,int>>,greater<>> pq;
         pq.push({0,i});
         dist[i]=0;
         while(!pq.empty()){
-            auto [wt,node]=pq.top();
+            auto[d,u]=pq.top();
             pq.pop();
-            if(wt>dist[node]) continue;
-            for(auto it: graph[node]){
-                int neigh=it.first;
-                long long  neighWt=it.second;
-                if(dist[node]+neighWt<dist[neigh]){
-                    dist[neigh]=dist[node]+neighWt;
-                    pq.push({dist[neigh],neigh});
+            if(d>dist[u]) continue;
+
+            for(auto [v,wt]:adj[u]){
+                if(dist[v]>dist[u]+wt){
+                    dist[v]=dist[u]+wt;
+                    pq.push({dist[v],v});
                 }
             }
+
         }
         return dist;
     }
     vector<int> minCost(int n, vector<int>& prices, vector<vector<int>>& roads) {
-        vector<vector<pair<int,long long>>> graph_empty(n),graph_loaded(n);
+        vector<vector<pair<int,long long>>> adj1(n),adj2(n);
 
-        int m=roads.size();
-        for(int i=0;i<m;i++){
-            
-                int u=roads[i][0];
-                int v=roads[i][1];
-                long long  cost=roads[i][2];
-                long long tax=roads[i][3];
-                graph_empty[u].push_back({v,cost});
-                graph_empty[v].push_back({u,cost});
-                graph_loaded[u].push_back({v,cost*tax});
-                graph_loaded[v].push_back({u,cost*tax});
-            
+        for(auto& road:roads){
+            int u=road[0];
+            int v=road[1];
+            long long cost=road[2];
+            long long tax=road[3];
+
+            // cost graph
+            adj1[u].push_back({v,cost});
+            adj1[v].push_back({u,cost});
+
+            //cost*tax graph
+            adj2[u].push_back({v,cost*tax});
+            adj2[v].push_back({u,cost*tax});
+             
         }
 
+        
         vector<int> ans(n);
 
         for(int i=0;i<n;i++){
 
-            vector<long long> go=dijkstra(n,i,graph_empty);
-            vector<long long > come=dijkstra(n,i,graph_loaded);
+            vector<long long> go=dijkstra(n,i,adj1);
+            vector<long long> come=dijkstra(n,i,adj2);
 
             long long best=LLONG_MAX;
             for(int j=0;j<n;j++){
-                long long total= go[j]+prices[j]+come[j];
+                long long total=go[j]+prices[j]+come[j];
                 best=min(best,total);
             }
             ans[i]=best;
