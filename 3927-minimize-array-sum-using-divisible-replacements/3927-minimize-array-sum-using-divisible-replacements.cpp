13e87fae1782fc42bf5f9826a769efe77a505625
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-05-10 11:03:56 +0530

    Time: 147 ms (81.88%), Space: 151.8 MB (97.83%) - LeetHub

--- 3927-minimize-array-sum-using-divisible-replacements/3927-minimize-array-sum-using-divisible-replacements.cpp
+++ 3927-minimize-array-sum-using-divisible-replacements/3927-minimize-array-sum-using-divisible-replacements.cpp
@@ -1,35 +1,25 @@
+const int MAXN=1e5+7;
+int cnt[MAXN];
 class Solution {
 public:
-    vector<int> getFactors(int n){
-        vector<int> factors;
-        for(int i=1;i*i<=n;i++){
-            if(n%i==0) {
-                factors.push_back(i);
-                if(n/i!=i) factors.push_back(n/i);
-            }
-        }
-        sort(factors.begin(),factors.end());
-        return factors;
-    }
     long long minArraySum(vector<int>& nums) {
         int n=nums.size();
 
-        unordered_map<int,int> umap;
-        for(int i=0;i<n;i++) umap[nums[i]]++;
-
-
+        for(auto it : nums)  cnt[it]++;
+        
         long long ans=0;
-        for(int i=0;i<n;i++){
-            vector<int> f=getFactors(nums[i]);
+       for(int i=1;i<MAXN;i++){
 
-            for(auto it: f){
-                if(umap.find(it)!=umap.end()){
-                    ans+=it;
-                    break;
+            if(cnt[i]){
+
+                for(int j=2*i;j<MAXN;j+=i){
+                    cnt[i]+=cnt[j];
+                    cnt[j]=0;
                 }
+                ans+=(cnt[i]*(long long ) i);
             }
-            
-        }   
-        return ans;
+       }
+       fill_n(cnt,MAXN,0);
+       return ans;
     }
 };
