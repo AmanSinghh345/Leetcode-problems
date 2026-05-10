e1737630496167f335017c1e372669aa6c983541
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-05-10 09:50:56 +0530

    Time: 0 ms (100%), Space: 30.6 MB (48.41%) - LeetHub

--- 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.cpp
+++ 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.cpp
@@ -3,9 +3,8 @@ public:
     vector<int> concatWithReverse(vector<int>& nums) {
         
         int n=nums.size();
-        vector<int> temp=nums;
-        reverse(nums.begin(),nums.end());
-        for(int i=0;i<n;i++) temp.push_back(nums[i]);
-        return temp; 
+        nums.reserve(2*n);
+        for(int i=n-1;i>=0;i--) nums.push_back(nums[i]);
+        return nums;
     }
 };
