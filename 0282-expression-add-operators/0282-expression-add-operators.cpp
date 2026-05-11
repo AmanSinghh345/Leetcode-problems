8b8ec1126dd94df78ff386927879ad689b8a9cbe
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-05-11 10:57:27 +0530

    Time: 438 ms (25.97%), Space: 93.5 MB (30.67%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.cpp
+++ 0282-expression-add-operators/0282-expression-add-operators.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
-    void find(string& s,int& target,int pos,long long  currVal,long long prev,string ds,vector<string>& ans){
-
-        //base case 
+    void find(string& s,int target,int pos,long long currVal,long long prev,string ds,vector<string>& ans){
         if(pos==s.size()){
-            if(currVal==target){
+            if(currVal==target) {
                 ans.push_back(ds);
-                return;
+                return ;
             }
         }
 
         for(int len=1;len<=s.size()-pos;len++){
+
             string part=s.substr(pos,len);
             long long num=stoll(part);
 
             if(part.size()>1 and part[0]=='0') break;
-
+            
             if(pos==0){
                 find(s,target,pos+len,num,num,part,ans);
             }
             else{
-                //place + 
                 find(s,target,pos+len,currVal+num,num,ds+"+"+part,ans);
-                //place -
                 find(s,target,pos+len,currVal-num,-num,ds+"-"+part,ans);
-                //place * 
                 find(s,target,pos+len,currVal-prev+prev*num,prev*num,ds+"*"+part,ans);
             }
-
         }
-
     }
     vector<string> addOperators(string num, int target) {
         vector<string> ans;
