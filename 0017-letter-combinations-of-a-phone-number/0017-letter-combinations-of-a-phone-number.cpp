1b640bfb959de281a888e75bb5e311be4dd554da
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-05-11 08:18:06 +0530

    Time: 0 ms (100%), Space: 9.2 MB (58.51%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    void find(int i,string ds,vector<string>& ans,string& digits,vector<string>& phone){
+        
+       
+        //base case 
+        if(i==digits.size()){
+            ans.push_back(ds);
+            return ;
+        }
+
+        
+         int d=digits[i]-'0';// char to int
+
+         for(int j=0;j<phone[d].size();j++){
+        //take 
+        find(i+1,ds+phone[d][j],ans,digits,phone);
+        
+         }
+
+    }
+    vector<string> letterCombinations(string digits) {
+        
+        vector<string> phone(10);
+        phone[2]="abc";
+        phone[3]="def";
+        phone[4]="ghi";
+        phone[5]="jkl";
+        phone[6]="mno";
+        phone[7]="pqrs";
+        phone[8]="tuv";
+        phone[9]="wxyz";
+
+        vector<string> ans;
+        string ds;
+        find(0,ds,ans,digits,phone);
+
+        return ans;
+
+    }
+};
