739b36c14167e01c0a0e438bd19c2e726411dc60
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-05-11 09:15:21 +0530

    Time: 190 ms (84.94%), Space: 10.2 MB (99.13%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,51 +1,33 @@
 class Solution {
 public:
-    bool find(int i,int j,int  next,vector<vector<char>>& board, string& word){
-        
+    bool find(int i,int j,int next,vector<vector<char>>& board,string& word){
+
         //base case
-        if(next==word.size()){
-            return true;
-        }
+        if(next==word.size()) return true;
+
+        if(i<0 || i>=board.size() || j<0 || j>=board[0].size()) return false;
+        if(word[next]!=board[i][j]) return false;
 
         char temp=board[i][j];
         board[i][j]='#';
 
-        int dx[]={-1,0,1,0};
-        int dy[]={0,1,0,-1};
+        bool found=find(i-1,j,next+1,board,word)||
+                    find(i,j+1,next+1,board,word)||
+                    find(i+1,j,next+1,board,word)||
+                    find(i,j-1,next+1,board,word);
 
-        for(int k=0;k<4;k++){
-            int ni=i+dx[k];
-            int nj=j+dy[k];
-            if(ni>=0 && ni<(int)board.size() && nj>=0 && nj<(int)board[0].size() && board[ni][nj]==word[next]){
-               
-                if(find(ni,nj,next+1,board,word)) return true;
-               
-            }
-        }
+        
         board[i][j]=temp;
-        return false;
+        return found;
+
     }
     bool exist(vector<vector<char>>& board, string word) {
         int m=board.size();
         int n=board[0].size();
-
-        vector<pair<int,int>> pos;
-
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(word[0]==board[i][j]) pos.push_back({i,j});
+                if(find(i,j,0,board,word)) return true;
             }
-        }
-        if(pos.size()==0) return false;
-        
-
-        for(int y=0;y<pos.size();y++){
-            int i=pos[y].first;
-            int j=pos[y].second;
-    
-            int next=1;
-       if( find(i,j,next,board,word)) return true;
-
         }
         return false;
     }
