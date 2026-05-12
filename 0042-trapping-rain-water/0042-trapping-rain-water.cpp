98144c6565b713bb4cd2b9b93703d5160b74f675
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-05-12 11:59:06 +0530

    Time: 0 ms (100%), Space: 25.9 MB (92.49%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,41 +1,32 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        
         int n=height.size();
-        int maxi=INT_MIN;
-        vector<int> maxiR(n),maxiL(n);
-        maxiR[n-1]=-1;
-        maxi=height[n-1];
-        for(int i=n-2;i>=0;i--){
-            if(maxi>height[i]){
-                maxiR[i]=maxi;
+        int l=0;
+        int r=n-1;
+        int leftMax=0,rightMax=0;
+        int ans=0;
+
+        while(l<r){
+            if(height[l]<height[r]){
+                if(height[l]>=leftMax){
+                    leftMax=height[l];
+                }
+                else{
+                    ans+=(leftMax-height[l]);
+                }
+                l++;
             }
             else{
-                maxiR[i]=-1;
-                maxi=height[i];
+                if(height[r]>=rightMax){
+                    rightMax=height[r];
+                }
+                else{
+                    ans+=(rightMax-height[r]);
+                }
+                r--;
             }
         }
-        maxiL[0]=-1;
-        maxi=height[0];
-        for(int i=1;i<n;i++){
-            if(maxi>height[i]){
-                maxiL[i]=maxi;
-            }
-            else{
-                maxiL[i]=-1;
-                maxi=height[i];
-            }
-        }
-        int contribution=0;
-        for(int i=0;i<n;i++){
-            if(maxiL[i]==-1 || maxiR[i]==-1) contribution+=0;
-            else{
-                int take=min(maxiR[i],maxiL[i]);
-                int contribute=take-height[i];
-                contribution+=contribute;
-            }
-        }
-        return contribution;
+        return ans;
     }
 };
