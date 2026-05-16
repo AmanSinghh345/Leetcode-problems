9b9ad3399767092e50dd009ffaba192a8f0915c9
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-05-16 17:21:15 +0530

    Time: 66 ms (18.53%), Space: 18.6 MB (92.28%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.cpp
@@ -5,8 +5,11 @@ public:
         int n=matrix[0].size();
 
         for(int i=0;i<m;i++){
+
+            if(target<matrix[i][0] || target>matrix[i][n-1]) continue;
             int l=0,r=n-1;
             while(l<=r){
+
                 int mid=l+(r-l)/2;
                 if(matrix[i][mid]==target ) return true;
                 else if(matrix[i][mid]<target) l=mid+1;
