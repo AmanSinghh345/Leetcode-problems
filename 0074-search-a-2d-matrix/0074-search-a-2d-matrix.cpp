704aaa276b060e17de504bc039e3e1ae03ca8442
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-05-16 16:56:11 +0530

    Time: 0 ms (100%), Space: 13.2 MB (77.72%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -4,29 +4,17 @@ public:
         int m=matrix.size();
         int n=matrix[0].size();
 
-          //find correct row 
-          //so i make take only 0th col element 
-          //find row where value is just minimum 
-           
-          int u=0,d=m-1;
-          while(u<d){
-            int mid=u+(d-u+1)/2;
-            if(matrix[mid][0]==target) return true;
-            else if(matrix[mid][0]<target) u=mid;
-            else d=mid-1;
-          }  
-
-        // we got row 
-        //now find 1d bs on this row 
-
-        int l=0,r=n-1;
+        int l=0;
+        int r=m*n-1;
         while(l<=r){
-            int mid=(l+r)/2;
-            if(matrix[u][mid]==target) return true;
-            else if(matrix[u][mid]<target) l=mid+1;
+            int mid=l+(r-l)/2;
+            int row=mid/n;
+            int col=mid%n;
+
+            if(matrix[row][col]==target) return true;
+            else if(matrix[row][col]<target) l=mid+1;
             else r=mid-1;
         }
-    return false;
-
+        return false;
     }
 };
