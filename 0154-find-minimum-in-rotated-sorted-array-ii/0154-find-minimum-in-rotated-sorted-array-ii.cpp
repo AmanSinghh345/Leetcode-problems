5a0495f8f520b767ebddccf3b14e744ff292e488
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-05-16 11:15:23 +0530

    Time: 0 ms (100%), Space: 16.2 MB (27.34%) - LeetHub

--- 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.cpp
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.cpp
@@ -1,37 +1,20 @@
 class Solution {
 public:
-    bool left(int l,int r,vector<int>& nums){
-        while(l<r){
-            int mid=(l+r)/2;
-            if(nums[mid]<nums[r]) return true;
-            if(nums[mid]>nums[r]) return false;
-            else{
-                return left(l,mid,nums)||left(mid+1,r,nums);
-            }
-        }
-        return false;
-    }
     int findMin(vector<int>& nums) {
-         int n=nums.size();
+        int n=nums.size();
         int l=0,r=n-1;
         while(l<r){
-            int mid=(l+r)/2;
-            if(nums[mid]==nums[r]){
-                if(left(l,mid,nums)){
-                    r=mid;
-                }
-                else{
-                    l=mid+1;
-                }
-            } 
-            else if(nums[mid]>nums[r]){
+            int mid=l+(r-l)/2;
+            if(nums[mid]>nums[r]){
                 l=mid+1;
             }
-            else{
+            else if(nums[mid]<nums[r]){
                 r=mid;
             }
+            else{
+                r--;
+            }
         }
-        int ans=min(nums[0],nums[l]);
-        return ans;
+        return nums[l];
     }
 };
