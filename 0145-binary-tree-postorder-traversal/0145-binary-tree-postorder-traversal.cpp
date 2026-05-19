940c379332f7cc2a7ef1e48613e50cba89aefa60
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-05-19 15:37:17 +0530

    Time: 3 ms (3.99%), Space: 11.1 MB (43.18%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.cpp
@@ -1,34 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
-        if (root == nullptr) return {};
-
-        vector<int> post;
-        stack<pair<TreeNode*, int>> st;
-        st.push({root, 1});
-
-        while (!st.empty()) {
-            auto it = st.top();
-            st.pop();
-
-            if (it.second == 1) {
-                it.second = 2;
-                st.push(it);   
-
-                if (it.first->left)
-                    st.push({it.first->left, 1});
-            }
-            else if (it.second == 2) {
-                it.second = 3;
-                st.push(it);
-
-                if (it.first->right)
-                    st.push({it.first->right, 1});
-            }
-            else {
-                post.push_back(it.first->val);
-            }
+        vector<int> ans;
+        if(root==NULL) return ans;
+        stack<TreeNode*> st1,st2;
+        st1.push(root);
+        while(!st1.empty()){
+            TreeNode* node=st1.top();
+            st1.pop();
+            st2.push(node);
+            if(node->left) st1.push(node->left);
+            if(node->right) st1.push(node->right);
         }
-        return post;
+        while(!st2.empty()){
+            ans.push_back(st2.top()->val);
+            st2.pop();
+        }
+        return ans;
     }
 };
