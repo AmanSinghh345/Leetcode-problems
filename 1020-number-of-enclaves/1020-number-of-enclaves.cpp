44ba4668fdeb8948a75516dee2e13c1d09385fb6
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-05-23 12:35:19 +0530

    Time: 0 ms (100%), Space: 28.7 MB (96.94%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -3,32 +3,28 @@ public:
     int numEnclaves(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> mat(m, vector<int>(n, -1));
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                mat[i][j]=grid[i][j];
-            }
-        }
+
+        
         queue<pair<int, int>> q;
         for (int i = 0; i < m; i++) {
 
             if (grid[i][0] == 1)
              {   q.push({i, 0});
-                mat[i][0]=0;
+                grid[i][0]=0;
              }
             if (grid[i][n - 1] == 1){
                 q.push({i, n - 1});
-                mat[i][n-1]=0;
+                grid[i][n-1]=0;
                 }
         }
         for (int j = 1; j < n-1; j++) {
             if (grid[0][j]){
                 q.push({0, j});
-                mat[0][j]=0;
+                grid[0][j]=0;
                 }
             if (grid[m - 1][j]){
                 q.push({m - 1, j});
-                mat[m-1][j]=0;
+                grid[m-1][j]=0;
                 }
         }
         int ans = 0;
@@ -42,8 +38,8 @@ public:
                 int nx = x + dx[i];
                 int ny = y + dy[i];
                 if (nx >= 0 && nx < m && ny >= 0 && ny < n &&
-                    mat[nx][ny] == 1) {
-                    mat[nx][ny] = 0;
+                    grid[nx][ny] == 1) {
+                    grid[nx][ny] = 0;
                     q.push({nx, ny});
                 }
             }
@@ -51,7 +47,7 @@ public:
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(mat[i][j]) ans++;
+                if(grid[i][j]) ans++;
             }
         }
         return ans;
