d875dae4a291b0e47d03c11d7ce29e39254094bb
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-05-25 13:54:01 +0530

    Time: 0 ms (100%), Space: 15 MB (40.58%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -6,24 +6,22 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    void recursioncall(TreeNode* root, int level, vector<int>& ans) {
-        if (root == NULL)
-            return;
-        if (level == ans.size())
-            ans.push_back(root->val);
-        recursioncall(root->right, level + 1, ans);
-        recursioncall(root->left, level + 1, ans);
+    void dfs(TreeNode* root,int level,vector<int>& ans){
+        if(root==NULL) return ;
+
+        if(level==ans.size()) ans.push_back(root->val);
+        dfs(root->right,level+1,ans);
+        dfs(root->left,level+1,ans);
     }
     vector<int> rightSideView(TreeNode* root) {
         vector<int> ans;
-        int level = 0;
-        recursioncall(root, level, ans);
+        if(root==NULL) return ans;
+        dfs(root,0,ans);
         return ans;
     }
 };
