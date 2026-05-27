679a0d89dccd5c513de606bf9b92704d559f5b1f
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-05-27 13:09:50 +0530

    Time: 1 ms (98.5%), Space: 22.6 MB (46.61%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,38 +1,60 @@
+class DisjointSet{
+    public : 
+    vector<int> parent,size;
+    DisjointSet(int n){
+        parent.resize(n);
+        size.resize(n,1);
+
+        for(int i=0;i<n;i++) parent[i]=i;
+    }
+
+    int find(int node){
+        if(parent[node]==node) return node;
+        return parent[node]=find(parent[node]);
+    }
+
+    void unionBySize(int a,int b){
+        int pa=find(a);
+        int pb=find(b);
+        if(pa==pb) return;
+        if(size[pa]<size[pb]){
+            size[pb]+=size[pa];
+            parent[pa]=pb;
+        }
+        else{
+            size[pa]+=size[pb];
+            parent[pb]=pa;
+    }
+    }
+};
 class Solution {
 public:
-    void dfs(int node,vector<int>& vis,vector<vector<int>>& adj){
-        vis[node]=1;
-        for(int neigh : adj[node]){
-            if(vis[neigh]==-1) dfs(neigh,vis,adj);
-        }
-        return;
-    }
     int removeStones(vector<vector<int>>& stones) {
-        int n=stones.size();
+        int maxRow=0,maxCol=0;
+        for(auto & stone : stones){
+            maxRow=max(maxRow,stone[0]);
+            maxCol=max(maxCol,stone[1]);
+        }
 
-        vector<vector<int>> adj(n);
+        int totalNodes=maxRow+maxCol+2;
 
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                int x1=stones[i][0],y1=stones[i][1];
-                int x2=stones[j][0],y2=stones[j][1];
+        DisjointSet ds(totalNodes);
 
-                if(x1==x2 || y1==y2){
-                    adj[i].push_back(j);
-                    adj[j].push_back(i);
-                }
-            }
+        unordered_set<int> usedNodes;
+
+        for(auto& stone : stones){
+            int row=stone[0];
+            int col=stone[1]+maxRow+1;
+
+            ds.unionBySize(row,col);
+            usedNodes.insert(row);
+            usedNodes.insert(col);
         }
 
         int components=0;
-        vector<int> vis(n,-1);
-
-        for(int i=0;i<n;i++){
-            if(vis[i]==-1){
-                components++;
-                dfs(i,vis,adj);
-            }
+        for(int x:usedNodes){
+            if(ds.find(x)==x) components++;
         }
-        return n-components;
+        return stones.size()-components;
     }
 };
