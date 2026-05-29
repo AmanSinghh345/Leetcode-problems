1139c55dd956521008049063cbaa84fc49bc1e06
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-05-29 14:47:23 +0530

    Time: 13 ms (95.46%), Space: 9.1 MB (99.18%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -2,33 +2,22 @@ class Solution {
 public:
     int minCut(string s) {
         int n=s.size();
-        vector<vector<bool>> isPallin(n,vector<bool> (n,false));
-
-        for(int len=1;len<=n;len++){
-            for(int i=0;i<=n-len;i++){
-                int j=i+len-1;
-                if(s[i]==s[j]){
-                    if(len<=2) isPallin[i][j]=true;
-                    else{
-                        isPallin[i][j]=isPallin[i+1][j-1];
-                    }
-                }
-            }
-        }
-
         vector<int> dp(n,INT_MAX);
-
         for(int i=0;i<n;i++){
-            if(isPallin[0][i]){
-                dp[i]=0;
+
+            for(int len=0 ; i-len>=0 && i+len<n && s[i-len]==s[i+len] ; len++ )
+            {
+                int l=i-len;
+                int r=i+len;
+                dp[r]=(l==0)?0:min(dp[r],dp[l-1]+1);
             }
-            else{
-                for(int k=1;k<=i;k++){
-                    if(isPallin[k][i] && dp[k-1]!=INT_MAX){
-                        dp[i]=min(dp[i],dp[k-1]+1);
-                    }
-                }
+            for(int len=0;i-len>=0 && i+len+1<n && s[i-len]==s[i+len+1];len++){
+                int l=i-len;
+                int r=i+len+1;
+                dp[r]=(l==0)?0:min(dp[r],dp[l-1]+1);
             }
+
+
         }
         return dp[n-1];
     }
