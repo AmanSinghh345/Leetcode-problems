23dba1f445a210f59475020011cd3144dfe12252
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-05-29 11:02:03 +0530

    Time: 19 ms (79.75%), Space: 15.3 MB (5.51%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -17,7 +17,19 @@ public:
         cuts.push_back(n);
         sort(cuts.begin(),cuts.end());
         int m=cuts.size();
-        vector<vector<long long >> dp(m,vector<long long > (m,-1));
-        return (int)f(1,(int)cuts.size()-2,cuts,dp);
+        vector<vector<long long >> dp(m,vector<long long > (m,0));
+        
+        for(int i=m-2;i>=1;i--){
+            for(int j=1;j<=m-2;j++){
+                if(i>j) continue;
+                int mini=INT_MAX;
+                for(int ind=i;ind<=j;ind++){
+                    int cost=cuts[j+1]-cuts[i-1]+dp[i][ind-1]+dp[ind+1][j];
+                    mini=min(mini,cost);
+                }
+                dp[i][j]=mini;
+            }
+        }
+        return dp[1][m-2];
     }
 };
