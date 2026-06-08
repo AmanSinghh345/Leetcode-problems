801b1a0465eeb304fc06fe6808f1d91a2c098ede
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-06-08 08:19:27 +0530

    Time: 0 ms (100%), Space: 10.7 MB (43.47%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
-        int prev2=0,prev1=0;
-        for(auto x:nums)
-        {
-            int curr=max(prev1,x+prev2);
-            prev2=prev1;
-            prev1=curr;
+    int find(int i,vector<int>& nums,vector<int>& dp){
+        if(i>=nums.size()) {
+            return 0;
         }
-        return prev1;
+        if(dp[i]!=-1) return dp[i];
+        //pick
+        int pick=nums[i]+find(i+2,nums,dp);
+        //not pick
+        int notpick=find(i+1,nums,dp);
+        return dp[i]=max(pick,notpick);
+
+    }
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp(n,-1);
+        return find(0,nums,dp);
     }
 };
