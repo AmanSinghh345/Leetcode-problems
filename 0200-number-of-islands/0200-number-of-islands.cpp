f2209a479538e8bb61313f9335d8a562d2e34fda
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-06-13 07:49:16 +0530

    Time: 30 ms (33.79%), Space: 19.5 MB (34.66%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
+
     int numIslands(vector<vector<char>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-        int cnt=0;
         int dx[]={-1,0,1,0};
         int dy[]={0,1,0,-1};
-
+        vector<vector<int>> vis(m,vector<int> (n,0));
+        queue<pair<int,int>> q;
+        int cnt=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]=='0') continue;
-                cnt++;
-                queue<pair<int,int>> q;
-                q.push({i,j});
-                grid[i][j]='0';
-                while(!q.empty()){
-                    auto [x,y]=q.front();
-                    q.pop();
+                if(grid[i][j]=='1' && !vis[i][j]){
+                    cnt++;
+                    q.push({i,j});
+                    while(!q.empty()){
+                        auto [x,y]=q.front();
+                        q.pop();
 
-                    for(int k=0;k<4;k++){
-                        int nx=x+dx[k];
-                        int ny=y+dy[k];
-                        if(nx>=0 && nx<m && ny>=0 && ny < n && grid[nx][ny]=='1'){
-                            q.push({nx,ny});
-                            grid[nx][ny]='0';
+                        for(int k=0;k<4;k++){
+                            int nx=x+dx[k];
+                            int ny=y+dy[k];
+                            if(nx>=0 && nx<m && ny>=0 && ny<n && grid[nx][ny]=='1' && !vis[nx][ny] ){
+                                q.push({nx,ny});
+                                vis[nx][ny]=1;
+                            }
                         }
                     }
                 }
-
             }
         }
         return cnt;
