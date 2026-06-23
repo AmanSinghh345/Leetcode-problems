6280947ed40c3728a02cde343fc2901985033ad8
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-06-23 12:07:35 +0530

    Time: 43 ms (73.45%), Space: 20.9 MB (73.91%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -3,27 +3,27 @@ public:
     int cherryPickup(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-        vector<vector<int>> prev(n,vector<int> (n,INT_MIN));
-        
+        vector<vector<int>> prev(n,vector<int> (n,-1));
         prev[0][n-1]=grid[0][0]+grid[0][n-1];
-
         for(int i=1;i<m;i++){
-            vector<vector<int>> curr(n,vector<int> (n,INT_MIN));
+            vector<vector<int>> curr(n,vector<int> (n,0));
+        
             for(int c1=0;c1<n;c1++){
                 for(int c2=0;c2<n;c2++){
-                    int cnt=(c1==c2)?grid[i][c1]:grid[i][c1]+grid[i][c2];
-                    int best=INT_MIN;
-                    for(int k=-1;k<=1;k++){
-                        for(int l=-1;l<=1;l++){
+                    curr[c1][c2]=(c1==c2)?grid[i][c1]:grid[i][c1]+grid[i][c2];
+                    int best=-1;
+                    for(int k=-1;k<2;k++){
+                        for(int l=-1;l<2;l++){
                             int nc1=c1+k;
                             int nc2=c2+l;
-                            if(nc1>=0 and nc1<n and nc2>=0 and nc2<n) {
+                            if(nc1>=0 && nc1<n && nc2>=0 && nc2<n && prev[nc1][nc2]!=-1){
                                 best=max(best,prev[nc1][nc2]);
                             }
                         }
                     }
-                    if(best!=INT_MIN)
-                    curr[c1][c2]=cnt+best;
+                    if(best==-1) curr[c1][c2]=-1;
+                    else
+                    curr[c1][c2]+=best;
                 }
             }
             prev=curr;
@@ -35,6 +35,5 @@ public:
             }
         }
         return ans;
-
     }
 };
