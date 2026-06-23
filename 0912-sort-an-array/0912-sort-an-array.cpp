26be1e35dac2974f35815417620d10164e15b625
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-06-23 15:39:51 +0530

    Time: 1 ms (99.87%), Space: 75.9 MB (60.31%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,47 +1,19 @@
 class Solution {
 public:
-    void merge(int l,int mid,int r,vector<int>& nums){
-        int n1=mid-l+1;
-        int n2=r-mid;
-        vector<int> left(n1),right(n2);
-        for(int i=0;i<n1;i++) left[i]=nums[l+i];
-        for(int j=0;j<n2;j++) right[j]=nums[mid+1+j];
-        int i=0,j=0,k=l;
-        while(i<n1 & j<n2){
-            if(left[i]<=right[j]){
-                nums[k]=left[i];
-                i++;
-                k++;
-            }
-            else {
-                nums[k]=right[j];
-                j++;
-                k++;
-            }
-        }
-        while(i<n1){
-            nums[k]=left[i];
-            i++;
-            k++;
-        }
-        while(j<n2){
-            nums[k]=right[j];
-            j++;
-            k++;
-        }
-    }
-    void mergeSort(int l,int r,vector<int>& nums){
-        if(l>=r) return ;
-        int mid=l+(r-l)/2;
-
-        mergeSort(l,mid,nums);
-        mergeSort(mid+1,r,nums);
-        merge(l,mid,r,nums);
-        return ;
-    }
     vector<int> sortArray(vector<int>& nums) {
+        int mini=*min_element(nums.begin(),nums.end());
+        int maxi=*max_element(nums.begin(),nums.end());
+        int range=maxi-mini+1;
+        vector<int> cntArr(range,0);
         int n=nums.size();
-        mergeSort(0,n-1,nums);
-        return nums;
+        for(int i=0;i<n;i++) cntArr[nums[i]-mini]++;
+
+        vector<int> ans;
+        for(int i=0;i<range;i++){
+            while(cntArr[i]--){
+                ans.push_back(i+mini);
+            }
+        }
+        return ans;
     }
 };
