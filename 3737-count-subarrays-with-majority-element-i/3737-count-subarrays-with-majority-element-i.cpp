4274b6cb053d7eb0da4c81604519d3b5bcf8cd4d
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-06-26 14:47:47 +0530

    Time: 4 ms (95.02%), Space: 43.3 MB (16.23%) - LeetHub

--- 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.cpp
@@ -1,20 +1,52 @@
 class Solution {
 public:
-    int countMajoritySubarrays(vector<int>& nums, int target) {
+    class Fenwick{
+        public:
+        int n;
+        vector<int> bits; 
+        Fenwick(int n){
+            this->n=n;
+            bits.assign(n+1,0);
+        }
+        void update(int idx,int val){
+            while(idx<=n){
+                bits[idx]+=val;
+                idx+=(idx&-idx);
+            }
+        }
+        long long query(int idx){
+            long long res=0;
+            while(idx>0){
+               res+= bits[idx];
+               idx-=(idx&-idx);
+            }
+            return res;
+        }
+    };
+    long long countMajoritySubarrays(vector<int>& nums, int target) {
         int n=nums.size();
-        long long cnt=0;
-        vector<long long > freq(n,0);
+      
+        vector<long long > prefix(n+1,0);
         long long  pref=0;
         for(int i=0;i<n;i++){
             if(nums[i]==target) pref++;
-            freq[i]=pref;
+            else pref--;
+            prefix[i+1]=pref;
         }
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-              long long cntTarget=freq[j];
-              if(i>0) cntTarget-=freq[i-1];
-               if(cntTarget*2>(j-i+1)) cnt++;
-            }
+
+        Fenwick fw(n);
+        vector<long long> vals=prefix;
+        sort(vals.begin(),vals.end());
+        vals.erase(unique(vals.begin(),vals.end()),vals.end());
+
+        auto getIndex=[&](long long x ){
+            return lower_bound(vals.begin(),vals.end(),x)-vals.begin()+1;
+        };
+        long long cnt=0;
+        for(int i=0;i<=n;i++){
+            int idx=getIndex(prefix[i]);
+            cnt+=fw.query(idx-1);
+            fw.update(idx,1);
         }
         return cnt;
     }
