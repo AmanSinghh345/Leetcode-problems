da29364865e130fe5abeaaeafc1e45ab3d2c8f02
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-06-28 18:55:54 +0530

    Time: 0 ms (100%), Space: 8.6 MB (54.08%) - LeetHub

--- 3270-find-the-key-of-the-numbers/3270-find-the-key-of-the-numbers.cpp
+++ 3270-find-the-key-of-the-numbers/3270-find-the-key-of-the-numbers.cpp
@@ -3,9 +3,9 @@ public:
     int generateKey(int num1, int num2, int num3) {
         int ans=0;
         int place=1;
-        for(int i=0;i<4;i++){
+        for(int i=0;i<4;i++,place*=10){
             ans+=(min({num1/place % 10  , num2/place % 10 , num3/place % 10 }))*place;
-            place*=10;
+           
         }
         return ans;
     }
