e5869e643944705c589af174ef86bc8c25534ddc
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-06-30 17:28:02 +0530

    Time: 0 ms (100%), Space: 10.9 MB (99.19%) - LeetHub

--- 1854-maximum-population-year/1854-maximum-population-year.cpp
+++ 1854-maximum-population-year/1854-maximum-population-year.cpp
@@ -1,44 +1,23 @@
 class Solution {
 public:
     int maximumPopulation(vector<vector<int>>& logs) {
-        vector<int> vals;
+        vector<int> prefix(101,0);
         for(int i=0;i<(int)logs.size();i++){
-                
-                int birth=logs[i][0];
-                int death=logs[i][1];
-                vals.push_back(birth);
-                vals.push_back(death);
+                int birth=logs[i][0]-1950;
+                int death=logs[i][1]-1950;
+
+                prefix[birth]++;
+                prefix[death]--;
         }
-
-        sort(vals.begin(),vals.end());
-        vals.erase(unique(vals.begin(),vals.end()),vals.end());
-
-        auto getIndex=[&](int x){
-                return lower_bound(vals.begin(),vals.end(),x)-vals.begin()+1;
-        };
-        unordered_map<int,int> umap;
-
-        vector<int> pref(vals.size()+1,0);
-        for(int i=0;i<logs.size();i++){
-            int birth=logs[i][0];
-            int death=logs[i][1];
-
-            int B_idx=getIndex(birth);
-            int D_idx=getIndex(death);
-            umap[B_idx]=birth;
-            umap[D_idx]=death;
-            pref[B_idx]++;
-            pref[D_idx]--;
-        }
-        int idx;
-        int maxi=0;
-        for(int i=1;i<pref.size();i++){
-            pref[i]+=pref[i-1];
-            if(pref[i]>maxi){
-                maxi=pref[i];
+        int maxi=prefix[0];
+        int idx=0;
+        for(int i=1;i<101;i++){
+            prefix[i]+=prefix[i-1];
+            if(prefix[i]>maxi) {
+                maxi=prefix[i];
                 idx=i;
             }
         }
-        return umap[idx];
+        return idx+1950;
     }
 };
