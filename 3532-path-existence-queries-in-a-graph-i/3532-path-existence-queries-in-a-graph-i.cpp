e3e90096f8bb85e09242a809fd637559cc445a39
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-07-09 18:45:28 +0530

    Time: 69 ms (22.19%), Space: 240.7 MB (18.63%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.cpp
@@ -1,21 +1,49 @@
 class Solution {
 public:
+    class DSU{
+        public:
+        int n;
+        vector<int> size;
+        vector<int> parent;
+        DSU(int n){
+            this->n=n;
+            size.assign(n,1);
+            parent.resize(n);
+            for(int i=0;i<n;i++) parent[i]=i;
+        }
+
+        int find(int node){
+            if(parent[node]==node) return node;
+            return parent[node]=find(parent[node]);
+        }
+
+        void UnionBySize(int a,int b){
+            int pa=find(a);
+            int pb=find(b);
+            if(pa==pb) return;
+            if(size[pa]>size[pb]){
+                parent[pb]=pa;
+                size[pa]+=size[pb];
+            }
+            else {
+                parent[pa]=pb;
+                size[pb]+=size[pa];
+            }
+            return;
+        }
+    };
     vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-        vector<int> component(n,0);
-        int comp=0;
-        for(int i=1;i<n;i++){
-            if(nums[i]-nums[i-1]>maxDiff) comp++;
-            component[i]=comp;
-        }
-
+       DSU dsu(n);
+       for(int i=1;i<n;i++){
+        if(nums[i]-nums[i-1]<=maxDiff) dsu.UnionBySize(i,i-1);
+       }  
         vector<bool> ans;
-        for(auto it : queries){
-            int u=it[0];
-            int v=it[1];
-
-            if(component[u]==component[v]) ans.push_back(true);
-            else ans.push_back(false);
-        }
-        return ans;
+       for(auto it : queries){
+        int u=it[0];
+        int v=it[1];
+        if(dsu.find(u)==dsu.find(v)) ans.push_back(true);
+        else ans.push_back(false);
+       }
+       return ans;
     }
 };
