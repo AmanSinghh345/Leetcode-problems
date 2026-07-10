860d88f2f092557ea76bee7d659703f4664207d9
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-07-10 20:47:30 +0530

    Time: 358 ms (31.58%), Space: 409.6 MB (14.47%) - LeetHub

--- 3534-path-existence-queries-in-a-graph-ii/3534-path-existence-queries-in-a-graph-ii.cpp
+++ 3534-path-existence-queries-in-a-graph-ii/3534-path-existence-queries-in-a-graph-ii.cpp
@@ -1,72 +1,71 @@
 class Solution {
 public:
-    vector<int> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
-        vector<pair<int,int>> arr;
-        for(int i=0;i<n;i++){
-            arr.push_back({nums[i],i});
+    vector<int> pathExistenceQueries(int n, vector<int>& nums, int maxDiff,
+                                     vector<vector<int>>& queries) {
+
+        int LOG = 1;
+        while ((1 << LOG) <= n)
+            LOG++;
+        vector<pair<int, int>> arr;
+        for (int i = 0; i < n; i++) {
+            arr.push_back({nums[i], i});
         }
-        sort(arr.begin(),arr.end());
-
-
-        vector<int> val(n),pos(n);
-        for(int i=0;i<n;i++){
-            val[i]=arr[i].first;
-            pos[arr[i].second]=i;
+        sort(arr.begin(), arr.end());
+        vector<int> vals(n);
+        vector<int> pos(n);
+        for (int i = 0; i < n; i++) {
+            vals[i] = arr[i].first;
+            pos[arr[i].second] = i;
         }
-        
-        int LOG=1;
-        while((1<<LOG)<=n) LOG++;
 
-        //fartest with one jump;
         vector<int> nxt(n);
-        int r=0;
-        for(int i=0;i<n;i++){
-            if(r<i) r=i;
-            while(r+1<n && val[r+1]-val[i]<=maxDiff){
+        int r = 0;
+        for (int i = 0; i < n; i++) {
+            if (r < i)
+                r = i;
+            while (r + 1 < n && vals[r + 1] - vals[i] <= maxDiff)
                 r++;
+            nxt[i] = r;
+        }
+
+        vector<vector<int>> up(n, vector<int>(LOG));
+        for (int i = 0; i < n; i++) {
+            up[i][0] = nxt[i];
+        }
+
+        for (int j = 1; j < LOG; j++) {
+            for (int i = 0; i < n; i++) {
+                up[i][j] = up[up[i][j - 1]][j - 1];
             }
-            nxt[i]=r;
         }
-
-        vector<vector<int>> up(n,vector<int>(LOG));
-        for(int i=0;i<n;i++){
-            up[i][0]=nxt[i];
-        }
-
-        for(int j=1;j<LOG;j++){
-            for(int i=0;i<n;i++){
-                up[i][j]=up[up[i][j-1]][j-1];
-            }
-        }
-
         vector<int> ans;
-        for(auto & q : queries){
-            int u=pos[q[0]];
-            int v=pos[q[1]];
+        for (auto& q : queries) {
+            int u = pos[q[0]];
+            int v = pos[q[1]];
 
-            if(u==v) {
+            if (u == v) {
                 ans.push_back(0);
                 continue;
             }
-            if(u>v) swap(u,v);
 
-            int curr=u;
-            int step=0;
+            int steps = 0;
+            if (u > v)
+                swap(u, v);
+            int curr = u;
 
-            for(int j=LOG-1;j>=0;j--){
-                
-                    if(up[curr][j]<v) {
-                        curr=up[curr][j];
-                        step+=(1<<j);
-                    }
-                
+            for (int j = LOG - 1; j >= 0; j--) {
+
+                if (up[curr][j] <v) {
+                    curr = up[curr][j];
+                    steps += (1 << j);
+                }
             }
-            if(up[curr][0]>=v) {
-                step++;
-                ans.push_back(step);
-            }
-            else ans.push_back(-1);
 
+            if (up[curr][0] >= v) {
+                steps++;
+                ans.push_back(steps);
+            } else
+                ans.push_back(-1);
         }
         return ans;
     }
