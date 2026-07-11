cd63a30512b6ee9683087ea826699601da7364fd
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-07-11 16:45:24 +0530

    Time: 3 ms (28%), Space: 15.6 MB (81.95%) - LeetHub

--- 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 0462-minimum-moves-to-equal-array-elements-ii/0462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -4,6 +4,7 @@ public:
         sort(nums.begin(),nums.end());
         int n=nums.size();
         int mid=n/2;
+        nth_element(nums.begin(),nums.begin()+mid,nums.end());
         int median=nums[mid];
         int diff=0;
         for(int x:nums) diff+=(abs(median-x));
