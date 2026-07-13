a96ef4a056186dedbd203be6f836339471640e04
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-07-13 13:33:58 +0530

    Time: 0 ms (100%), Space: 8.5 MB (18.24%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -3,23 +3,12 @@ public:
     vector<int> sequentialDigits(int low, int high) {
         vector<int> ans;
         string s="123456789";
-        string a=to_string(low);
-        string b=to_string(high);
-        int len1=a.size();
-        int len2=b.size();
-        cout<<"len 1 : "<<len1<<endl;
-        cout<<"len 2 : "<<len2 <<endl;
-        for(int w=len1;w<=len2;w++){
-            int l=0;
-            while(l+w<10){
-                cout<<l<<endl;
-                string get=s.substr(l,w);
-                cout<<get<<endl;
-                int num=stoi(get);
-                if(num>=low && num<=high) ans.push_back(num);
-                l++;
+        for(int len=2;len<=9;len++){
+            for(int i=0;i+len<=9;i++){
+                int val=stoi(s.substr(i,len));
+                if(val>=low && val<=high) ans.push_back(val);
             }
-        }
-        return ans;
+        } 
+       return ans;
     }
 };
