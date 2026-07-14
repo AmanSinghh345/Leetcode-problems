1475c1695a7e5b94fd4467a92276625267552600
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-07-14 15:12:08 +0530

    Time: 701 ms (63.48%), Space: 22.9 MB (98.26%) - LeetHub

--- 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -1,26 +1,38 @@
 class Solution {
 public:
-    int mod=1e9+7;
-    long long  dp[205][205][205];
-    int generateSubsequence(int i,int seq1,int seq2,vector<int>& nums){
-        //base case
-        if(i==nums.size()){
-           if(seq1==seq2){
-                return 1;       
-           }
-           return 0;
-        }
-        if(dp[i][seq1][seq2]!=-1) return dp[i][seq1][seq2];
-        int np=(generateSubsequence(i+1,seq1,seq2,nums))%mod;
-        int p=((generateSubsequence(i+1,(int)gcd(seq1,nums[i]),seq2,nums))%mod+(generateSubsequence(i+1,seq1,(int)gcd(seq2,nums[i]),nums))%mod)%mod;
-        return dp[i][seq1][seq2]=((np)%mod+p)%mod;
-        
-    }
+    
     int subsequencePairCount(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
-       int seq1=0,seq2=0; 
-       int total=0;
-        total=(total+(generateSubsequence(0,seq1,seq2,nums))%mod)%mod;
-       return total-1;
+        int dp[2][205][205];
+        memset(dp,-0,sizeof(dp));
+        int n=nums.size();
+        int total=0;
+        int mod=1e9+7;
+        //base case 
+        for(int seq=0;seq<205;seq++){
+            dp[n%2][seq][seq]=1;
+        }
+
+        for(int i=n-1;i>=0;i--){
+             int curr=i%2;
+             int next=(i+1)%2;
+            int next_gcd[205];
+            for(int s=0;s<205;s++){
+                next_gcd[s]=gcd(s,nums[i]);
+            }
+             for(int seq1=0;seq1<205;seq1++){
+                for(int seq2=0;seq2<205;seq2++){
+                    long long  ways=dp[next][seq1][seq2];
+
+                  
+                    ways+=dp[next][next_gcd[seq1]][seq2];
+                    
+                    ways+=dp[next][seq1][next_gcd[seq2]];
+                    
+                    dp[curr][seq1][seq2]=ways%mod;
+                }
+             }
+        }
+       
+       return (dp[0][0][0]-1+mod)%mod;
     }
 };
