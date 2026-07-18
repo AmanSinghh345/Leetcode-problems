af16d09df5dc1a9d86fcdf6af3b7f9f11e736e9a
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-07-18 17:41:04 +0530

    Time: 0 ms (100%), Space: 7.9 MB (12.24%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
         if(n<=0) return false;
-        if(1==__builtin_popcount(n)) return true;
+        if(__builtin_popcount(n)==1) return true;
         return false;
     }
 };
