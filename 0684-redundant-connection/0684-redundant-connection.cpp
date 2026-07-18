d73f1662ce647e7c84898a01f9def7dc760b9d0c
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-07-18 18:56:56 +0530

    Time: 0 ms (100%), Space: 13.3 MB (44.72%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,49 +1,40 @@
 class Solution {
 public:
-    int find(vector<int> &parent,int x){
-    
-    if(parent[x]==x){
-        return x;
-    }
-   
-    return parent[x]=find(parent,parent[x]);
-}
-
-bool Union(vector<int>&parent,vector<int>&size,int a,int b){
-    int pa=find(parent,a);
-    int pb=find(parent,b);
-    if(pa==pb){
-        return true;
-    }
-    if(size[pa]>=size[pb]){
-      
-        size[pa]+=size[pb];
-        parent[pb]=pa;
-       
-    }
-    else{
-       
-        size[pb]+=size[pa];
-        parent[pa]=pb;
-        
-
-    }
-    return false;
-}
+    class Disjoint{
+        public:
+        vector<int> parent;
+        vector<int> size;
+        Disjoint(int n){
+            parent.resize(n);
+            for(int i=0;i<n;i++) parent[i]=i;
+            size.assign(n,1);
+        }
+        int find(int node){
+            if(parent[node]==node) return node;
+            return parent[node]=find(parent[node]);
+        }
+        void UnionBySize(int a,int b){
+            int pa=find(a);
+            int pb=find(b);
+            if(pa==pb) return;
+            if(size[pb]>size[pa]) swap(pa,pb);
+            size[pa]+=size[pb];
+            parent[pb]=pa;
+            return;
+        }
+    };
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n=edges.size();
-        vector<int> parent(n+1); 
-
-vector<int> size(n+1,1);
-for(int i=0;i<=n;i++){
-    parent[i]=i;
-   
-}
-for(int i=0;i<n;i++){
-    if(Union(parent,size,edges[i][0],edges[i][1])){
-        return {edges[i][0],edges[i][1]};
-    }
-}
-return {};
+        Disjoint dsu(n);
+        for(auto& edge:edges){
+            int u=edge[0];
+            int v=edge[1];
+         
+            if(dsu.find(u-1)==dsu.find(v-1)) {
+                return {u,v};
+            }
+            dsu.UnionBySize(u-1,v-1);
+        }
+        return {};
     }
 };
