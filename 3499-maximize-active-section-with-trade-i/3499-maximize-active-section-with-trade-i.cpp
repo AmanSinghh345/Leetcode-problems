3b14a3500eea5c48e4300ce5868e8d90b756df06
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-07-22 15:08:15 +0530

    Time: 58 ms (68.3%), Space: 55 MB (58.04%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
     int maxActiveSectionsAfterTrade(string s) {
+        int n=s.size();
         int ans=0;
         for(char c:s) {
             if(c=='1') ans++;
         }
-        int n=s.size();
-        int maxi=0,prev=INT_MIN;
-        int curr=0;
-        int i=0;
-        while(i<n){
-            if(s[i]=='0'){
-            int j=i+1;
-            while(j<n && s[i]==s[j]){
-                j++;
-            }
-            curr=j-i;
-            maxi=max(maxi,curr+prev);
-            prev=curr;
-            i=j;
-            continue;
-            }
-            i++;
 
+        int prev=INT_MIN;
+        int curr=0;
+        int maxi=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='0'){
+                int j=i+1;
+                while(j<n && s[j]==s[i]){
+                    j++;
+                  
+                }
+                curr=j-i;
+        
+                maxi=max(maxi,prev+curr);
+                prev=curr;
+                i=j;
+                continue;
+            }
+          
         }
-        ans+=maxi;
-        return ans;
+        cout<<ans<<" "<<maxi<<endl;
+        return ans+maxi;
     }
 };
