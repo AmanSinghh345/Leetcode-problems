e4f243ca51c1969e05f037bf53f3dd3036afcb25
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-07-23 14:44:47 +0530

    Time: 7 ms (96.4%), Space: 69 MB (78.25%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,41 +1,28 @@
 class Solution {
 public:
-
-    int quickselect(vector<int>& nums,int low,int high,int target){
-           while(low<=high){
-            int randomInd=low+rand()%(high-low+1);
-            int pivot=nums[randomInd];
-
-            int lt=low;
-            int mid=low;
-            int gt=high;
-            while(mid<=gt){
-                if(nums[mid]<pivot){
-                    swap(nums[mid],nums[lt]);
-                    lt++;
-                    mid++;
-                }
-                else if(nums[mid]>pivot){
-                    swap(nums[mid],nums[gt]);
-                    gt--;
-                }
-                else mid++;
-            }
-
-          
-
-            if(target<lt) high=lt-1;
-            else if(target>gt) low=gt+1;
-            else return nums[target];
-
-           }
-           return -1;
-
-    }
     int findKthLargest(vector<int>& nums, int k) {
-        int n=nums.size();
-
-        // n-k is the postiion we want
-       return  quickselect(nums,0,n-1,n-k);
+        int n = nums.size();
+        k = n - k;
+        auto quickSort = [&](auto&& quickSort, int l, int r) -> int {
+            if (l == r) {
+                return nums[l];
+            }
+            int i = l - 1, j = r + 1;
+            int x = nums[(l + r) >> 1];
+            while (i < j) {
+                while (nums[++i] < x) {
+                }
+                while (nums[--j] > x) {
+                }
+                if (i < j) {
+                    swap(nums[i], nums[j]);
+                }
+            }
+            if (j < k) {
+                return quickSort(quickSort, j + 1, r);
+            }
+            return quickSort(quickSort, l, j);
+        };
+        return quickSort(quickSort, 0, n - 1);
     }
 };
