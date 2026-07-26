26bf3a8268d7422f0a42eba3baf34815696cd0d0
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-07-26 19:26:17 +0530

    Time: 15 ms (92.24%), Space: 9 MB (97.1%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -4,15 +4,18 @@ public:
         int n1=text1.size();
         int n2=text2.size();
       
-        vector<int> prev(n2+1,0);
+        vector<int> dp(n2+1,0);
+       
         for(int i=1;i<=n1;i++){
-            vector<int> curr(n2+1,0);
+               int prevDiag=0;
             for(int j=1;j<=n2;j++){
-                if(text1[i-1]==text2[j-1]) curr[j]=prev[j-1]+1;
-                else curr[j]=max(prev[j],curr[j-1]);
+                int temp=dp[j];
+                if(text1[i-1]==text2[j-1]) dp[j]=prevDiag+1;
+                else dp[j]=max(dp[j],dp[j-1]);
+                prevDiag=temp;
             }
-            prev=curr;
+        
         }
-        return prev[n2];
+        return dp[n2];
     }
 };
