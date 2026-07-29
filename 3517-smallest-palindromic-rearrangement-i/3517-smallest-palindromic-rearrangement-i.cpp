28e465257ce778eec0a5987a14796e2f073a2d2d
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-07-29 13:14:47 +0530

    Time: 69 ms (43.82%), Space: 160.2 MB (5.21%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -3,36 +3,21 @@ public:
     string smallestPalindrome(string s) {
         int n=s.size();
         vector<int> freq(26,0);
-        for(char c:s) freq[c-'a']++;
-
-        for(int x: freq) cout<<x<<" ";
-        cout<<endl;
-        bool foundOdd=false;
+        for(char c: s) freq[c-'a']++;
+        bool found=false;
         char ch;
-        string ans="";
         string left="",right="";
         for(int i=0;i<26;i++){
             if(freq[i]==0) continue;
-            if(freq[i]&1) {
-                foundOdd=true;
-                ch='a'+i;
+            if(freq[i]&1){
+                found=true;
+                ch=('a'+i);
             }
-
-                int half=freq[i]/2;
-         
-                string temp=string(half,'a'+i);
-              
-                cout<<temp<<endl;;
-                left=left+temp;
-                right=temp+right;
-
-     
-            
+            int half=freq[i]/2;
+            left=left+string(half,('a'+i));
+            right=string(half,('a'+i))+right;
         }
-        if(foundOdd){
-            left+=ch;
-        }
-         ans=left+right;
-        return ans;
+        if(found) left+=ch;
+        return left+right;
     }
 };
