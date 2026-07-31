e839c12e3f1dc502620a8da4da62881f689d6c6c
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-07-31 14:08:44 +0530

    Time: 3 ms (66.36%), Space: 15 MB (12%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,12 +3,10 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n=nums.size();
         unordered_map<int,int> umap;
+        int pref=0;
         for(int i=0;i<n;i++){
-            int x=nums[i];
-            if(umap.count(target-x)){
-                return {i,umap[target-x]};
-            }
-            umap[x]=i;
+            if(umap.count(target-nums[i])) return {umap[target-nums[i]],i};
+            umap[nums[i]]=i;
         }
         return {-1,-1};
     }
