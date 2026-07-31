7cd40570cbcad0afbbca818a9bd9e52b19f1fc2a
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-07-31 16:04:25 +0530

    Time: 13 ms (77.96%), Space: 24.2 MB (93.36%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,35 +1,20 @@
 class Solution {
 public:
-    int mod=1e9+7;
-    vector<int> generatePrimes(int n){
-        int limit=10000;
-        vector<bool> isPrime(limit,true);
-        vector<int> res;
-        isPrime[0]=isPrime[1]=false;
-        for(int i=2;i<limit && res.size()<n ;i++){
-            if(isPrime[i]){
-                res.push_back(i);
-                for(int j=1LL*i*i;j<limit;j+=i) isPrime[j]=false;
-            }
-        }
-        return res;
-    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        vector<int> primes=generatePrimes(26);
-        
-        unordered_map<long long, vector<string>> umap;
-        vector<vector<string>> ans ;
-        int n=strs.size();
-        for(const auto &s:strs){
-            
-            long long key=1;
-            for(const auto& c : s){
-                key=(key*1LL*primes[c-'a'])%mod;
-            }
-            umap[key].push_back(s);
+        unordered_map<string,vector<string>> umap;
+        for(string& s:strs){
+            string p=s;
+            sort(p.begin(),p.end());
+            umap[p].push_back(s);
         }
-        for(const auto &[a,b]:umap){
-            ans.push_back(b);
+        int n=umap.size();
+        vector<vector<string>> ans(n);
+        int i=0;
+        for(auto & it : umap){
+            for(string & s:it.second){
+                 ans[i].push_back(s);
+            }
+            i++;
         }
         return ans;
     }
