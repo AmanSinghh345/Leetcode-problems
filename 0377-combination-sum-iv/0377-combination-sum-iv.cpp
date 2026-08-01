33bed4e3f532b0d38573441835ef24b78b2eff87
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-08-01 13:32:16 +0530

    Time: 4 ms (28.86%), Space: 9.4 MB (21.42%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -11,10 +11,18 @@ public:
         }
         return dp[target]=take;
     }
+    int mod=1e8+7;
     int combinationSum4(vector<int>& nums, int target) {
-        vector<int> dp(target+1,-1);
+        int n=nums.size();
+        vector< unsigned long long > dp(target+1,0);
         dp[0]=1;
-        find(target,nums,dp);
+        for(int i=1;i<=target;i++){
+            for(int j=0;j<n;j++){
+                if((i-nums[j])>=0) 
+                dp[i]+=dp[i-nums[j]];
+               
+            }
+        }
         return dp[target];
     }
 };
