72fff91e3b4a7f620e22702ad94c080725424010
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-08-01 15:21:14 +0530

    Time: 50 ms (56.96%), Space: 13.3 MB (57.49%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int numSquares(int n) {
+        vector<int> dp(n+1,INT_MAX);
+        dp[0]=0;
+        for(int i=1;i<=n;i++){
+            int j=1;
+            while(i-j*j>=0){
+                if(dp[i-j*j]!=INT_MAX) dp[i]=min(dp[i],1+dp[i-j*j]);
+                j++;
+            }
+        }
+        return (dp[n]==INT_MAX?-1:dp[n]);
+    }
+};
