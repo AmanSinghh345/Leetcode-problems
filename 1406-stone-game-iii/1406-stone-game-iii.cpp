89bb5351ab8cd22c302db9f2bbe68d2cb6cf8448
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-08-03 16:57:03 +0530

    Time: 16 ms (78.93%), Space: 136.1 MB (76.04%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    int find(int i,vector<int>& stoneValue,vector<int>& dp){
-        int n=stoneValue.size();
-        if(i>=n) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int res=INT_MIN;
-        int val=0;
-        for(int j=i;j<=min(i+2,n-1);j++){
-             val+=stoneValue[j];
-            res=max(res,val-find(j+1,stoneValue,dp));
-        }
-        return dp[i]=res;
-    }
+   
     string stoneGameIII(vector<int>& stoneValue) {
         int n=stoneValue.size();
-        vector<int> dp(n,-1);
-        find(0,stoneValue,dp);
+        vector<int> dp(n,0);
+        for(int i=n-1;i>=0;i--){
+            int res=INT_MIN;
+            int val=0;
+            for(int j=min(i,n-1);j<=min(i+2,n-1);j++){
+                val+=stoneValue[j];
+                int diff=0;
+                if(j+1<n) diff=dp[j+1];
+                res=max(res,val-diff);
+            }
+            dp[i]=res;
+        }
         if(dp[0]>0) return "Alice";
         else if(dp[0]==0) return "Tie";
         return "Bob";
