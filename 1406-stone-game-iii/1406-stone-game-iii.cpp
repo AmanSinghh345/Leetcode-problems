b8871dab32dddd3f1b5b20d01241bd04cdede67b
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-08-03 17:01:37 +0530

    Time: 14 ms (83.23%), Space: 130.1 MB (98.37%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -3,17 +3,19 @@ public:
    
     string stoneGameIII(vector<int>& stoneValue) {
         int n=stoneValue.size();
-        vector<int> dp(n,0);
+        vector<int> dp(3,0);
         for(int i=n-1;i>=0;i--){
             int res=INT_MIN;
             int val=0;
             for(int j=min(i,n-1);j<=min(i+2,n-1);j++){
                 val+=stoneValue[j];
                 int diff=0;
-                if(j+1<n) diff=dp[j+1];
+                if(j+1<n) diff=dp[j-min(i,n-1)];
                 res=max(res,val-diff);
             }
-            dp[i]=res;
+            dp[2]=dp[1];
+            dp[1]=dp[0];
+            dp[0]=res;
         }
         if(dp[0]>0) return "Alice";
         else if(dp[0]==0) return "Tie";
