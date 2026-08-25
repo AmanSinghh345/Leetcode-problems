3d69bb5fbb28d8f612de9eafdd7315710cf54e51
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-08-25 12:06:52 +0530

    Time: 0 ms (100%), Space: 24.6 MB (56.21%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        unordered_map<int,int> umap;
-        for(int x:nums) umap[x]++;
+        vector<int> freq(101,0);
+        for(int x:nums) freq[x]++;
         
-        for(int i=1;;i++){
-                 int val=k*i; 
-            if(umap.count(val)==0) return val;
-       
+        int mul=k;
+        while(mul<=100 and freq[mul]){
+            mul+=k;
         }
-        return -1;
+        return mul;
     }
 };
