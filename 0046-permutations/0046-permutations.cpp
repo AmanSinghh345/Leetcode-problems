a544c14ea00b994dff95329df60cecf0d164fa72
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-08-25 20:54:07 +0530

    Time: 0 ms (100%), Space: 11.8 MB (13.61%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-    void sol(vector <int> nums,vector<vector<int>>& ans,int index){
-        if(index>=nums.size())
-           { 
+    void find(int ind,vector<int> nums,vector<vector<int>>& ans){
+        if(ind==nums.size()){
             ans.push_back(nums);
             return ;
-           }
-           for(int i=index;i<nums.size();i++){
-            swap(nums[index],nums[i]);
-            sol(nums,ans,index+1);
-            swap(nums[index],nums[i]);
-           }
+        }
+        for(int i=ind;i<nums.size();i++){
+            swap(nums[ind],nums[i]);
+            find(ind+1,nums,ans);
+            swap(nums[ind],nums[i]);
+        }
     }
- 
     vector<vector<int>> permute(vector<int>& nums) {
+       
         vector<vector<int>> ans;
-        int index=0;
-        sol(nums,ans,index);
+        int ind=0;
+        find(ind,nums,ans);
         return ans;
     }
 };
