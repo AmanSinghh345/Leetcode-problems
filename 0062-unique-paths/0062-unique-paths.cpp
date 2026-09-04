1a52bca5c5da2a6c624fe6b07d24689245fc0863
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-09-04 23:01:16 +0530

    Time: 0 ms (100%), Space: 9.3 MB (54.36%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    long long nCr(int n,int r){
-        r=min(r,n-r);
-        long long res=1;
-        for(int i=1;i<=r;i++){
-            res=(res*(n-r+i))/i;
-        }
-        return res;
-    }
     int uniquePaths(int m, int n) {
-        return nCr(m+n-2,m-1);
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        for(int j=0;j<n;j++) dp[0][j]=1;
+        for(int i=0;i<m;i++) dp[i][0]=1;
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=dp[i][j-1]+dp[i-1][j];
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
