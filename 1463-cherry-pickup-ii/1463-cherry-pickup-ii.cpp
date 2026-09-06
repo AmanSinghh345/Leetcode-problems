e63e781b1ebc67b1f8f9ab1ba426c859cb6d0e9b
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-09-06 15:06:58 +0530

    Time: 36 ms (84.19%), Space: 21 MB (30.55%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,39 +1,27 @@
 class Solution {
 public:
+    int f(int i,int j1,int j2,int m,int n,vector<vector<int>>& grid,vector<vector<vector<int>>>& dp ){
+         if(j1<0 || j2<0 || j1>=n || j2>=n) return INT_MIN;
+         if(i==m-1){
+            if(j1==j2) return grid[i][j1];
+            else return grid[i][j1]+grid[i][j2];
+         }
+         if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
+         //transitions 
+         int maxi=INT_MIN;
+         for(int d1=-1;d1<=1;d1++){
+            for(int d2=-1;d2<=1;d2++){
+                
+                maxi=max(maxi,f(i+1,j1+d1,j2+d2,m,n,grid,dp));
+            }
+         }
+        if(j1==j2) return dp[i][j1][j2]= grid[i][j1]+maxi;
+        return dp[i][j1][j2]= grid[i][j1]+grid[i][j2]+maxi;
+    }
     int cherryPickup(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-        vector<vector<int>> prev(n,vector<int> (n,-1));
-        prev[0][n-1]=grid[0][0]+grid[0][n-1];
-        for(int i=1;i<m;i++){
-            vector<vector<int>> curr(n,vector<int> (n,0));
-        
-            for(int c1=0;c1<n;c1++){
-                for(int c2=0;c2<n;c2++){
-                    curr[c1][c2]=(c1==c2)?grid[i][c1]:grid[i][c1]+grid[i][c2];
-                    int best=-1;
-                    for(int k=-1;k<2;k++){
-                        for(int l=-1;l<2;l++){
-                            int nc1=c1+k;
-                            int nc2=c2+l;
-                            if(nc1>=0 && nc1<n && nc2>=0 && nc2<n && prev[nc1][nc2]!=-1){
-                                best=max(best,prev[nc1][nc2]);
-                            }
-                        }
-                    }
-                    if(best==-1) curr[c1][c2]=-1;
-                    else
-                    curr[c1][c2]+=best;
-                }
-            }
-            prev=curr;
-        }
-        int ans=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                ans=max(ans,prev[i][j]);
-            }
-        }
-        return ans;
+        vector<vector<vector<int>>> dp(m,vector<vector<int>>(n,vector<int>(n,-1)));
+        return f(0,0,n-1,m,n,grid,dp);
     }
 };
