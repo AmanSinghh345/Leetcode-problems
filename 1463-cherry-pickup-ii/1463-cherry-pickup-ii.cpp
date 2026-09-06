2ea39d8f09728e15420d852cb35f72657a052180
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-09-06 15:51:31 +0530

    Time: 19 ms (98.21%), Space: 13.3 MB (99.02%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -4,9 +4,9 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         const int NEG=-1e9;
-        vector<vector<vector<int>>> dp(
-            m, vector<vector<int>>(n, vector<int>(n,NEG)));
-        dp[0][0][n - 1] = grid[0][0] + grid[0][n - 1];
+        vector<vector<int>> prev(n, vector<int>(n,NEG));
+        vector<vector<int>> curr(n,vector<int>(n,NEG));
+        prev[0][n - 1] = grid[0][0] + grid[0][n - 1];
         int j1 = 0, j2 = n - 1;
         for (int i = 1; i < m; i++) {
             for (int j1 = 0; j1 < n; j1++) {
@@ -18,23 +18,24 @@ public:
                             int J2 = j2 + d2;
                             if (J1 < 0 || J2 < 0 || J1 >= n || J2 >= n)
                                 continue;
-                            maxi = max(maxi, dp[i - 1][J1][J2]);
+                            maxi = max(maxi, prev[J1][J2]);
                         }
                     }
                     if(maxi==NEG) continue;
                     if (j1 == j2)
-                        dp[i][j1][j2]= grid[i][j1]+maxi;
+                        curr[j1][j2]= grid[i][j1]+maxi;
                     else
-                        dp[i][j1][j2] = grid[i][j1] + grid[i][j2]+maxi;
+                        curr[j1][j2] = grid[i][j1] + grid[i][j2]+maxi;
 
                         
                 }
             }
+            prev.swap(curr);
         }
         int ans=NEG;
         for(int j1=0;j1<n;j1++){
             for(int j2=0;j2<n;j2++){
-                ans=max(ans,dp[m-1][j1][j2]);
+                ans=max(ans,prev[j1][j2]);
             }
         }
   
