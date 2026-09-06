30964efd0edbeb135e9f53e28234b0c54fa0518d
Aman Singh <148769177+AmanSinghh345@users.noreply.github.com>
2026-09-06 14:33:23 +0530

    Time: 0 ms (100%), Space: 12.6 MB (83.64%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -3,19 +3,12 @@ public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int m=triangle.size();
         int n=m;
-        vector<int> dp(m);
-        for(int j=0;j<m;j++){
-            dp[j]=triangle[m-1][j];
-        }
-
         for(int i=m-2;i>=0;i--){
-            
-          
             for(int j=0;j<triangle[i].size();j++){
-                dp[j]=triangle[i][j]+min(dp[j],dp[j+1]);
+             
+                triangle[i][j]+=min(triangle[i+1][j],triangle[i+1][j+1]);
             }
-            
         }
-        return dp[0];
+        return triangle[0][0];
     }
 };
